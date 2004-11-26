1b56bdce418119d2d6e2cdcaa1d7292a23526082
Alberto Garcia <wdpgaara@lg.ehu.es>
2004-11-26 23:04:11 +0000

    Siesta-1.1 Release -- completed on cvs rel-1-1-branch Changes and fixes in preparation of the 1.1 release.
    * Fixed mismatched MPI block in siesta.F (related to PDOS change).
      file: siesta.F
    
    * Total DOS file is now just *.DOS.
    
    * Better output for PDOS file. Used "xml" format to ease (?) 
      processing (evolving ideas).
      files: pdos.f pdosk.f 
    
    * Added new "h2o_dos" test in Src/Tests.
    
    * Added "xml" output for .ion data.
      files: xml.f basis_io.F radial.f initatom.f
    
    * Committed Bug fix for O(N) calculations (Julian Gale)
      file: gradient.F Src/Tests/Makefile (enabled h2o_orderN)
    
    * Added new sgi64-mpi_fermat.make file (Julian Gale)
    
    * Array shape fixes in xc.f (cosmetic) (Julian Gale)
    
    * Added top README file.
    
    * Added user.list (Please update), build_parallel.sh, and other 
      release-related files, and updated some others. These files are
      not part of any distribution.
    
    * Updated CONDITIONS (list of developers)
    
    * Many User Guide updates (J. Junquera, E. Artacho, A. Garcia)
    
    * Removed .vps, out.fdf, and siesta.size files from Examples directory.
    
    * Updated .out files in Src/Tests and Examples/*/Out
    
    * Removed  Util/PyAtom/pdos.py script (experimental -- not in release)

---
+++ build_parallel.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Creates the 'parallel' distribution of siesta.
+#
+#  This script should be run on an "EXPORTED" directory:
+#
+#  cvs export { -r <appropriate tag>, -d <date> } [ -d directory ] siesta
+#
+
+dir=siesta-1.1p
+
+if [ -d $dir ] ; then
+      echo "Directory exists"
+      exit
+fi
+
+mkdir $dir
+echo "Copying Pseudo Examples Docs Util Src ..."
+cp -rp Pseudo Examples Docs Util Src $dir
+cp -rp README send-bug $dir
+rm -rf $dir/Docs/Tech
+rm -rf $dir/Src/Include
+
+echo ""
+echo "**** Remember to include the .ps.gz User guide in 2up form"
+
