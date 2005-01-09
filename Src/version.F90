84f689465b54f89e3a5de66f9629fea8c51179b9
Alberto Garcia <wdpgaara@lg.ehu.es>
2005-01-09 17:19:49 +0000

    Siesta 1.4.35 -- removed old denchar source Version: 1.4.35 Date: 2005/01/08 16:20 GMT Title: Removed old Denchar source Author: Alberto Garcia <wdpgaara@lg.ehu.es>
    Removed all files under Util/Denchar/Src.
    (The source for Denchar has been in the top directory
    for a while)
    
    Changes to version.F90 and CHANGES.

--- Src/version.F90
+++ Src/version.F90
@@ -15,9 +15,9 @@ implicit none
 
 ! After it is done, this file should be commited.
 
-integer, dimension(3), save  :: num_version = (/1,4,33/)
+integer, dimension(3), save  :: num_version = (/1,4,35/)
 character(len=80), parameter :: version_str =  &
-"SIESTA 1.4.33  -- [Fix bugs wavefuncs]--patch-15 (05 Jan 2005)" 
+"SIESTA 1.4.35  -- [Removed old Denchar src] (08 Jan 2005)" 
 
 end module version_info
 !================================================================
