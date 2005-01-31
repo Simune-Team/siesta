728a9c5d94d481cf7b9014471beb6bac358e87e3
Alberto Garcia <wdpgaara@lg.ehu.es>
2005-01-31 16:25:01 +0000

    Siesta 1.4.37 -- Mesh subdivisions control from input Version: 1.4.37 Date: 2005/01/25 03:00 GMT Title: Mesh subdivisions control from input Author: Julian Gale <julian@power.curtin.edu.au>
    The value of nsm, which was previously a parameter, can now be
    adjusted from the input deck using the "MeshSubDivisions" option.
    The order of array deallocation in dhscf.F has also been reversed.
    
    Files:
    
    dhscf.F, meshsubs.F, version.F90, CHANGES, siesta.tex

--- Src/version.F90
+++ Src/version.F90
@@ -15,9 +15,9 @@ implicit none
 
 ! After it is done, this file should be commited.
 
-integer, dimension(3), save  :: num_version = (/1,4,36/)
+integer, dimension(3), save  :: num_version = (/1,4,37/)
 character(len=80), parameter :: version_str =  &
-"SIESTA 1.4.36  -- [Grdsam array size fix] (25 Jan 2005)" 
+"SIESTA 1.4.37  -- [Mesh subdivision from input] (31 Jan 2005)" 
 
 end module version_info
 !================================================================
