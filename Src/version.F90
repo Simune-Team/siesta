52eb6e38d0d9b45c93d4f521bb78927eecbe2a29
Alberto Garcia <wdpgaara@lg.ehu.es>
2005-02-04 08:34:43 +0000

    Siesta 1.4.38 -- O(N) extrapolation fix Version: 1.4.38 Date: 2005/02/04 05:30 GMT Title: O(N) extrapolation fix Author: Julian Gale <julian@power.curtin.edu.au>
    The order N code was failing in parallel when the structure
    of the Hamiltonian changed between geomtry steps. The code
    has been modified to solve this. The arrays cold / listcold
    now only keep the rows for which the current node has principal
    responsibility for. The extrapolation and I/O of LWFs now
    acts just on this part of C and is then globalised to all
    processors.
    
    Files:
    
    atomlwf.F      Dimensions of cold/listcold decreased by using nbasisloc
    egandd.F       Reallocation of arrays now occurs when iopt = 0 and
                   not just on the first call. The copy=.true. and
                   shrink=.false. attributes have also been added.
    extrapolon.F   Now acts only on 1->nbasisloc rather than 1->nbasisCloc
    iolwf.F        Now acts only on 1->nbasisloc rather than 1->nbasisCloc
                   and nbasisloc is added as a pased argument.
    ordern.F       Handling of cold/listcold changed and call to 
                   globaliseC added after extrapolon call.
    version.F90, CHANGES

--- Src/version.F90
+++ Src/version.F90
@@ -15,9 +15,9 @@ implicit none
 
 ! After it is done, this file should be commited.
 
-integer, dimension(3), save  :: num_version = (/1,4,37/)
+integer, dimension(3), save  :: num_version = (/1,4,38/)
 character(len=80), parameter :: version_str =  &
-"SIESTA 1.4.37  -- [Mesh subdivision from input] (31 Jan 2005)" 
+"SIESTA 1.4.38  -- [O(N) extrapolation fix] (4 Feb 2005)" 
 
 end module version_info
 !================================================================
