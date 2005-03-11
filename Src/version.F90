4bb2f3896cab80ff334ae3ed14862728ba399a2a
Alberto Garcia <wdpgaara@lg.ehu.es>
2005-03-11 10:40:14 +0000

    Siesta 1.4.45 --  Diag tag snag Version: 1.4.45 Date: 2005/03/10 03:00 GMT Title: Diag tag snag Author: Julian Gale <julian@power.curtin.edu.au>
    Changes:
    
    A couple of incorrect tags have been updated to reflect the fact
    that they should now be prefixed with "Diag." - spotted by
    Nichols.
    
    siesta.tex      ParallelOverK -> Diag.ParallelOverK
    redata.F        DivideAndConquer -> Diag.DivideAndConquer
    version.F90 / CHANGES

--- Src/version.F90
+++ Src/version.F90
@@ -15,9 +15,9 @@ implicit none
 
 ! After it is done, this file should be commited.
 
-integer, dimension(3), save  :: num_version = (/1,4,44/)
+integer, dimension(3), save  :: num_version = (/1,4,45/)
 character(len=80), parameter :: version_str =  &
-"SIESTA 1.4.44  -- [FSM dealloc order] (8 Mar 2005)" 
+"SIESTA 1.4.45  -- [Diag tag snag] (10 Mar 2005)" 
 
 end module version_info
 !================================================================
