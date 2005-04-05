1629b874e7cc48d356f87b792088758689aecfb9
Alberto Garcia <wdpgaara@lg.ehu.es>
2005-04-05 20:19:18 +0000

    Siesta 1.4.50 -- Parallel diag np/q fix Version: 1.4.50 Date: 2005/03/29 07:30 GMT Title: Parallel diag np/q fix Author: Julian Gale <julian@power.curtin.edu.au>
    The variables np/nq were being used without initialisation in some
    cases of parallel diagonalisation, according to options. This has
    been rectified, as well as removing the variable "iter" which was 
    unused.
    
    Files:
    
    cdiag.F    np/nq set and iter removed
    rdiag.F    np/nq set and iter removed
    version.F90 / CHANGES as per usual

--- Src/version.F90
+++ Src/version.F90
@@ -15,9 +15,9 @@ implicit none
 
 ! After it is done, this file should be commited.
 
-integer, dimension(3), save  :: num_version = (/1,4,49/)
+integer, dimension(3), save  :: num_version = (/1,4,50/)
 character(len=80), parameter :: version_str =  &
-"SIESTA 1.4.49  -- [CG init re-fix again] (23 Mar 2005)" 
+"SIESTA 1.4.50  -- [Parallel diag np/q fix] (29 Mar 2005)" 
 
 end module version_info
 !================================================================
