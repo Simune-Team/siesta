17e4deba0dd8bd1f3d08f453958ed43ef86b7610
Alberto Garcia <wdpgaara@lg.ehu.es>
2005-04-08 13:20:28 +0000

    add code for abort with NAG compiler Patches applied:
     * twhite@siesta.arch--2004/siesta-devel--twhite--1.4--patch-22
       Allow for formatted DM files.
    
     * twhite@siesta.arch--2004/siesta-devel--twhite--1.4--patch-23
       Fix permissions

--- Src/nag.f
+++ Src/nag.f
@@ -6,3 +6,10 @@ C
       call cpu_time(timereal)
       TIME = timereal
       END
+
+      subroutine abort(str)
+      use f90_unix, only: local_abort=>abort
+      character(len=*), optional, intent(in) :: str
+
+      call local_abort(str)
+      end subroutine abort
