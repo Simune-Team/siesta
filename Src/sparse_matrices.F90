a935a36fcf3a603fa2080588cd43d5ae5aaebdf2
Alberto Garcia <albertog@icmab.es>
2007-02-08 11:15:51 +0000

    New stub module for sparse matrices A stub module to hold the sparse matrices ('sparse_matrices') has been created.  It just holds the relevant variables, without any attempt to create derived types yet.
    (Work done at BSC in the afternoon of Feb 7)

---
+++ Src/sparse_matrices.F90
@@ -0,0 +1,12 @@
+MODULE sparse_matrices
+  use precision
+  implicit none
+  integer, pointer :: listh(:), listhold(:), listhptr(:), listhptrold(:), &
+                      numh(:), numhold(:)
+
+  real(dp), pointer :: Dold(:,:), Dscf(:,:), Dscfsave(:,:), &
+                       Eold(:,:), Escf(:,:), H(:,:)
+
+  real(dp), pointer :: H0(:), S(:)
+
+END MODULE sparse_matrices
