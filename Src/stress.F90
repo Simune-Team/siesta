f2138a27d90d38834a737fbda24aa40cb925ac47
Alberto Garcia <albertog@icmab.es>
2007-02-18 23:08:23 +0000

    Clarification of the scope of the stress variables A number of changes to rationalize the use of 'stress':
    * New routine 'overlap': a simplified version of overfsm adequate
    for calling from state_init, where only the overlap matrix is needed,
    and the stress was zero.
    
    * Routine setup_hamiltonian now globalizes the stress, and calls
    the 'twobody' routine. Variable 'stressl' made into a local.
    
    * local_DOS, and the writing of charge and potentials, do no need
    to use the 'stress' variable at all.

--- Src/stress.F90
+++ Src/stress.F90
@@ -5,12 +5,10 @@ MODULE m_stress
   ! Constrained stress tensor
   real(dp):: cstress(3,3)
 
-  ! Total stress tensor, includin kinetic components
+  ! Total stress tensor, including kinetic components
   real(dp):: tstress(3,3) 
 
   ! Stress tensor = d_E/d_strain
   real(dp):: stress(3,3) 
 
-  ! Local-mode part of the stress tensor
-  real(dp):: stressl(3,3) 
 END MODULE m_stress
