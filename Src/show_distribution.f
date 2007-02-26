86f777f005cc6637caff5c92cd3ac5caee98a810
Alberto Garcia <albertog@icmab.es>
2007-02-26 08:21:36 +0000

    Fixes for troublesome bugs in reference code Retrofitted from -devel--reference.
    Patches applied:
    
     * siesta@uam.es--2006/siesta-devel--reference--2.1--patch-31
       Parallel bug fix and cleanup in writewave.F
    
     * siesta@uam.es--2006/siesta-devel--reference--2.1--patch-42
       zval wrongly treated as MPI_integer in broadcast_basis
    
     * siesta@uam.es--2006/siesta-devel--reference--2.1--patch-43
       Wrap io_close for MPI in iohs.F
    
     * siesta@uam.es--2006/siesta-devel--reference--2.1--patch-45
       Clean up handling of ntote in ksv and optical
    
     * siesta@uam.es--2006/siesta-devel--reference--2.1--patch-46
       Fix allocation of temporary array in savepsi in parallel

---
+++ Src/show_distribution.f
@@ -0,0 +1,39 @@
+      subroutine show_distribution()
+      use parallel, only: Nodes
+      use parallelsubs
+      use siesta_geom, only: na_u
+      use atomlist, only: lasto
+      use sys, only: die
+
+      implicit none
+
+      integer io, Node, nuotot, gio, iu, localorbs
+      logical  :: abort_flag
+
+      nuotot = lasto(na_u)
+
+      call io_assign(iu)
+      open(unit=iu,file="PARALLEL_DIST",form="formatted",
+     $     status="replace",
+     $     action="write",position="rewind")
+
+      abort_flag = .false.
+      do Node = 0, Nodes-1
+         call getNodeOrbs(nuotot,Node,Nodes,localorbs)
+         write(iu,*) "Node ", Node, " handles ", localorbs, " orbitals."
+         if (localorbs == 0) abort_flag = .true.
+!         do io = 1, localorbs
+!            call LocalToGlobalOrb(io,Node,Nodes,gio)
+!            write(iu,*) gio
+!         enddo
+      enddo
+      call io_close(iu)
+      if (abort_flag) then
+          write(6,"(a)") "Some processors are idle. Check PARALLEL_DIST"
+          write(6,"(a)")
+     $         "You have too many processors for the system size !!!"
+!          call die()
+       endif
+
+      end subroutine show_distribution
+
