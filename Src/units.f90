e3280ab5d0f8a713843f041ddf826c05416677dc
Alberto Garcia <albertog@icmab.es>
2007-03-08 19:21:16 +0000

    First implementation of basis_enthalpy calculation New file BASIS_ENTHALPY contains the "effective basis enthalpy" of Anglada et al. The pressure is determined from the fdf symbol "BasisPressure", with a 0.2 GPa default.
    Further work: different weights to different species.

--- Src/units.f90
+++ Src/units.f90
@@ -19,9 +19,24 @@ module units
 
   implicit none
 
+!  The easy way to make sense of units conversion:
+
+!  real(dp), parameter :: Bohr   = 1.0_dp
+!  real(dp), parameter :: Rydberg = 1.0_dp
+!  real(dp), parameter :: Femtosecond = 1.0_dp
+!
+!  Ang = Bohr / 0.529177
+!   eV = Rydberg / 13.60580
+!  Joule = eV / 1.6e-19_dp
+!  Meter = Ang / 1.0e-10_dp
+!  Pascal = Joule/Meter**2
+!   kBar  = Pascal * 1.0e4
+!   .... and so on.
+
   real(dp), parameter :: Ang    = 1._dp / 0.529177_dp
   real(dp), parameter :: eV     = 1._dp / 13.60580_dp
   real(dp), parameter :: kBar   = 1._dp / 1.47108e5_dp
+  real(dp), parameter :: GPa    = kBar * 10
   real(dp), parameter :: Kelvin = eV / 11604.45_dp
   real(dp), parameter :: Debye  = 0.393430_dp
   real(dp), parameter :: amu    = 2.133107_dp
