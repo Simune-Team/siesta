57cb22a8f3190c2c8d55f498d4953057c6314978
Alberto Garcia <albertog@icmab.es>
2007-03-14 22:31:49 +0000

    VPATH-aware compilation for multiple executable versions Added Src/obj_setup.sh script, to be called from any directory on which the build is going to take place, and modified slightly some makefiles to make them VPATH-proof.  The Tests directory is replicated in the "build" directory.
    (The pristine Src directory should be "clean", without objects, or else
    the compilation in the build directories will get confused)
    
    Example. From the top Siesta directory:
    
    mkdir Objs
    cd Objs
    sh ../Src/obj_setup.sh
    ../Src/configure --enable-fast
    make
    cd Tests
    make clean; make
    cd    # Now a parallel version
    mkdir ParaObjs
    cd ParaObjs
    sh ../Src/obj_setup.sh
    cp ../Src/Sys/parallel.make ./arch.make
    make
    cd Tests
    make clean; make

---
+++ Src/obj_setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+##set -x
+#
+# Get absolute path of this script, as that will be the Src directory to use
+# as reference when copying files.
+# 
+#
+srcdir=$(
+cd -P -- "$(dirname -- "$0")" &&
+pwd -P
+)
+# The above construct is more robust than:  srcdir=$(dirname $0)
+# (It will work if $0 is "../Src", since we want an *absolute* path
+#
+user_specified_dir=$(dirname $0)
+testdir=$(dirname $srcdir)/Tests
+#
+destdir=$(pwd)
+#
+# Replicate the hierarchy of makefiles
+#
+for i in wxml xmlparser NetCDF MPI Libs fdf ; do
+    mkdir $i
+    cp ${srcdir}/$i/*akefile ${destdir}/$i
+done
+#
+sed "s#VPATH=\.#VPATH=${srcdir}#g" ${srcdir}/Makefile > ${destdir}/Makefile
+
+#
+# Tests directory
+# Create a list of files and use tar to process the list and copy the files
+# to the destination directory
+#
+( cd ${testdir} ; cd .. ; find Tests  \
+              -path *Reference -prune -o  \
+              -path *work -prune      -o  \
+              -path *.arch-ids  -prune -o -print \
+              | tar -cf - --no-recursion -T- )   | ( cd ${destdir} ; tar xf -)
+#
+# Now make a symbolic link in the destination directory
+#
+ln -s ${destdir} ${destdir}/Src
+#
+echo " *** Compilation setup done. "
+echo " *** Remember to copy an arch.make file or run configure as:"
+echo "    ${user_specified_dir}/configure [configure_options]"
