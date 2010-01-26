1fcc20be0478ddd0f57a27ae0b3dcee58f9b10ce
Alberto Garcia <albertog@icmab.es>
2010-01-26 17:19:32 +0100

    Jan 26-2010 update: Some docs; Cosmetics. Matrix io...
    Snapshot of state of Rogeli's code on Jan 26, 2010.
    
    Incomplete log:
    
    * Some documentation of grid routines.
    
    * Updates to Jacobi-Davidson.
    
    * Matrix market io (mmio)
    
    * Cosmetics, including retabbing of molecular mechanics and other
      routines.
    
    * Addition of a "reset" routine in module radial.f

---
+++ Src/walltime.c
@@ -0,0 +1,16 @@
+unsigned long long walltime( )
+{
+  unsigned int         HightB, HightA, Low;
+  unsigned long long   tt;
+
+  do
+  {
+    asm volatile("mftbu %0" : "=r"(HightB) );
+    asm volatile("mftb  %0" : "=r"(Low) );
+    asm volatile("mftbu %0" : "=r"(HightA) );
+  }
+  while (HightB != HightA);
+
+  tt = ((unsigned long long)HightA<<32) | ((unsigned long long)Low);
+  return tt;
+}
