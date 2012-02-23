7439d672602d8de1d116b33915b2e6fb129cc5b2
Alberto Garcia <albertog@icmab.es>
2012-02-23 10:32:22 +0100

    Update of Andrei Postnikov's utilities to V0.4
    Added also the fmpdos program, an alternative to pdosxml.
    
    added:
      Util/Contrib/APostnikov/00README
      Util/Contrib/APostnikov/fmpdos.f
      Util/Contrib/APostnikov/test_mdc.f
    modified:
      Util/Contrib/APostnikov/Makefile
      Util/Contrib/APostnikov/README
      Util/Contrib/APostnikov/eig2bxsf.f
      Util/Contrib/APostnikov/md2axsf.f
      Util/Contrib/APostnikov/rho2xsf.f
      Util/Contrib/APostnikov/vib2xsf.f
      Util/Contrib/APostnikov/wraxsf1.f
      Util/Contrib/APostnikov/wraxsf2.f

--- Util/Contrib/APostnikov/wraxsf1.f
+++ Util/Contrib/APostnikov/wraxsf1.f
@@ -14,12 +14,13 @@ C
       implicit none
       integer ii2,io1,is1,mdfirst,mdlast,mdstep,nat,nz(nat),mdmod,
      .        ii,jj,istep,nstep,idum,nbox,ibox,iat
-      double precision cc_ang(3,3),cc_bohr(3,3),cc_velo(3,3),
-     .                 coord(3,nat),veloc(3,nat),coort(3),
+      double precision cc_ang(3,3),cc_bohr(3,3),cc_velo(3,3),cc_md(3,3),
+     .                 coord(3,nat),veloc(3,nat),coort(3),coorf(3),
      .                 obox(3),rbox(3,3),rinv(3,3),b2ang
       parameter (b2ang=0.529177)   !  Bohr to Angstroem
+      parameter (is1=13)
       logical varcel
-      character symbol*2
+      character lett*1,symbol*2
       external fillbox
 
       rewind (ii2) 
@@ -27,7 +28,8 @@ C
 C --- write selected atoms first into a scratch file (is1), for the case
 C     there are zero. Then the label 'ATOMS' with no  atoms following
 C     will crash XCrySDen.
-      open (is1,form='formatted',status='scratch')
+C     open (is1, file='tmpfil',form='formatted',status='scratch')
+      open (is1,               form='formatted',status='scratch')
 C
       write (io1,"('ANIMSTEPS',i6)") (mdlast-mdfirst)/mdstep+1
       write (io1,"('CRYSTAL')")
@@ -64,7 +66,46 @@ C         ... with cc_ang as read from the XV file
             endif
           endif  !  if write this MD step
         enddo  !  do istep
-      elseif (mdmod.eq.2) then  ! (read data from ANI, coord. in Ang)
+
+      elseif (mdmod.eq.2) then  ! (read data from MD_CAR, coord. in Ang)
+        do istep = 1,nstep
+          read (ii2,'(a1)') lett  !  "---" // trim(slabel) //"---"
+          read (ii2,'(a1)') lett  !  "       1.0"
+          read (ii2,'(3f16.9)') cc_md  ! -- lattice vectors in Ang (each step)
+          read (ii2,'(a1)') lett  !  List of atoms: modify!!!
+          read (ii2,'(a1)') lett  !  "Direct"
+          if ((istep.ge.mdfirst).and.
+     .        (istep.le.mdlast).and.
+     .        (mod(istep-mdfirst,mdstep).eq.0)) then
+! ---       keep this MD step; make selection of atoms to fill the box
+            do iat=1,nat
+              read (ii2,'(3f16.9)') coorf ! -- fractional coordinates 
+! ---         reconstruct cartesian coordinates in Ang:
+              coord(:,iat) = coorf(1)*cc_md(:,1) +
+     +                       coorf(2)*cc_md(:,2) +
+     +                       coorf(3)*cc_md(:,3)
+            enddo
+            call fillbox(is1,obox,rbox,rinv,cc_md,nat,coord,nbox)
+            write (io1,"(i6,'  1')") nbox
+            if (nbox.gt.0) then
+              write (io1,"('PRIMCOORD',i6)") (istep-mdfirst)/mdstep+1
+              write (io1,"(i6,'  1')") nbox
+              rewind (is1)
+              do ibox = 1,nbox
+                read  (is1,201) iat,     (coort(jj),jj=1,3)
+                write (io1,201) nz(iat), (coort(jj)-obox(jj),jj=1,3)
+              enddo
+            else
+              write (6,*) ' Empty box for MD step ',istep,' !'
+            endif
+          else   !  skip this MD step 
+            do iat=1,nat
+              read (ii2,'(3f16.9)') coorf ! -- dummy read, not used 
+            enddo
+          endif  !  if write this MD step
+        enddo  !  do istep
+
+      elseif (mdmod.eq.3) then  ! (read from ANI, coord. in Ang)
         do istep = 1,nstep
           read (ii2,301) nat
           do iat=1,nat
@@ -89,6 +130,7 @@ C           coordinates in ANI are in Angstroem, and so passed to fillbox
           endif  !  if write this MD step
         enddo  !  do istep
       endif
+      close (is1)
       return
   201 format (i4,3f20.8)
   301 format(i5,/)
