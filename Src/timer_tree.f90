032dba7bc95ec873f05e3b4f2a752f84b878acd8
Alberto Garcia <albertog@icmab.es>
2013-01-31 10:34:45 +0100

    Document timer_tree module. Use 64-bit integers for time counter.
    Added documentation and hooks to timer_tree to make it almost
    completely independent of Siesta.
    
    Added new stand-alone 'die' routine (not in a module) that can
    be called from libraries and low-level modules.
    
    The maximum elapsed time without wrap-around in system_clock can
    be increased significantly by using 64-bit integers in the calls.
    This has been implemented.
    
    added:
      Src/die.F
    modified:
      Src/Makefile
      Src/m_walltime.f90
      Src/timer_tree.f90

--- Src/timer_tree.f90
+++ Src/timer_tree.f90
@@ -1,20 +1,47 @@
 module m_timer_tree
 
-implicit none
+  !
+  ! Timing framework that knows about the tree structure among
+  ! timed sections: an enclosing section has sub-sections as
+  ! 'children', and this relationship is preserved in the report.
+  ! Future extensions include the generation of a (possibly sorted)
+  ! flat report, and the accumulation of timings for specific classes
+  ! of sections.
+  !
+  ! This module keeps a "global_section" which is the parent of all
+  ! first-level sections.
+  !
+  ! Exported routines, all accepting a string (section name) as argument:
+  !
+  !  timer_on    :   Flags the section as 'active' and (resumes) timing it.
+  !  timer_off   :   Removes the 'active' flag and computes the elapsed time.
+  !  timer_report:   Produces a report (as of now it is a full report)
+  !
+  !  The timing is based by default on calls to wall_time()
+  !  This behavior can be changed by setting the variable 'use_walltime' to
+  !  .false.
+  ! 
+  !  Implementation notes: 
+  !    - The fixed-size array for the children is not robust enough, and
+  !      should be replaced by a more flexible structure, or at least by
+  !      a re-sizable pointer or allocatable array.
+  !      
+  !
+  ! Alberto Garcia, January 2013, re-using some pieces of code by Jose Soler
+  ! 
+  implicit none
 
-integer, parameter  :: dp = selected_real_kind(10,100)
-integer, parameter  :: NMAX_CHILDREN = 10
-integer, parameter  :: maxLength = 40
+  integer, parameter  :: dp = selected_real_kind(10,100)
+  integer, parameter  :: NMAX_CHILDREN = 10
+  integer, parameter  :: maxLength = 40
 
-! Derived type to hold time data
+  ! Derived type to hold time data
   type times_t
-    private
-    character(len=maxLength):: name=' '  ! Name of program or code section
-    integer :: nCalls=0           ! Number of calls made to the program
-    real(dp):: totTime=0          ! Total time for this program
-    real(dp):: comTime=0          ! Communications time
-    real(dp):: lastTime=0         ! Total time in last call
-    real(dp):: lastComTime=0      ! Communications time in last call
+     private
+     character(len=maxLength):: name=' '  ! Name of program or code section
+     integer :: nCalls=0           ! Number of calls made to the program
+     real(dp):: totTime=0          ! Total time for this program
+     real(dp):: lastTime=0         ! Total time in last call
   end type times_t
 
   type section_t
@@ -24,7 +51,7 @@ integer, parameter  :: maxLength = 40
      integer       :: nchildren = 0
      type(section_t), dimension(:), pointer :: child
   end type section_t
-     
+
   type(section_t), pointer :: global_section => null()
   type(section_t), pointer :: last_active => null()
 
@@ -40,8 +67,15 @@ integer, parameter  :: maxLength = 40
   public :: timer_on, timer_off, timer_report
   private
 
+  interface
+     subroutine die(str)
+       character(len=*), intent(in), optional :: str
+     end subroutine die
+  end interface
+
 CONTAINS
 
+  !------------------------------------------------
   subroutine timer_on(secname)
     character(len=*)    :: secname
 
@@ -50,57 +84,68 @@ CONTAINS
     ! Use an extra enclosing level for everything,
     ! so that multiple user "trees" can be supported
 
-      if (.not. associated(global_section)) then
-         allocate(global_section)
-         p => global_section
-         p%active = .true.
-         pd => p%data
-         pd%name = "global_section"
-         pd%nCalls = pd%nCalls + 1
-         last_active => p
-         call current_time( treal )
-         pd%lastTime = treal
-      endif
+    if (.not. associated(global_section)) then
+       allocate(global_section)
+       p => global_section
+       p%active = .true.
+       pd => p%data
+       pd%name = "global_section"
+       pd%nCalls = pd%nCalls + 1
+       last_active => p
+       call current_time( treal )
+       pd%lastTime = treal
+    endif
 
-      ! Find proper place
-         p => last_active
-         if (p%nchildren==0) then
-            allocate(p%child(NMAX_CHILDREN))
-         endif
-         call child_index(secname,p%child,loc)
-         if (loc == 0) then
-            if (p%nchildren == NMAX_CHILDREN) STOP "too many children"
-            p%nchildren = p%nchildren + 1
-!            print *, "New child: " // trim(secname) // " of " // trim(p%data%name)
-            p=>p%child(p%nchildren)
-            p%caller => last_active
-         else
-            p => p%child(loc)
-            p%caller => last_active
-         endif
+    ! Find proper place
+    p => last_active
+    if (p%nchildren==0) then
+       allocate(p%child(NMAX_CHILDREN))
+    endif
+    call child_index(secname,p%child,loc)
+    if (loc == 0) then
+       ! New child
+       if (p%nchildren == NMAX_CHILDREN) then
+          ! re_allocate in future
+          call die("too many children")
+       endif
+       p%nchildren = p%nchildren + 1
+       !print *, "New child: " // trim(secname) // " of " // trim(p%data%name)
+       p=>p%child(p%nchildren)
+       p%caller => last_active
+    else
+       ! Existing child
+       p => p%child(loc)
+       p%caller => last_active
+    endif
 
-               
-      p%active = .true.
-      pd => p%data
-      pd%name = secname
-      pd%nCalls = pd%nCalls + 1
-      last_active => p
-      ! Find present CPU time and convert it to double precision
-      call current_time( treal )
-      pd%lastTime = treal
+    p%active = .true.
+    pd => p%data
+    pd%name = secname
+    pd%nCalls = pd%nCalls + 1
+    last_active => p
+    ! Find present time
+    call current_time( treal )
+    pd%lastTime = treal
 
-    end subroutine timer_on
+  end subroutine timer_on
 
+  !------------------------------------------------
   subroutine timer_off(secname)
     character(len=*)    :: secname
 
+    character(len=256) :: msg
+
     p => last_active
     if (trim(p%data%name) /= trim(secname)) then
-       print *, "on/off mismatch: ", trim(p%data%name), " ", trim(secname)
-       stop
+       write(msg,"(a,a,a,a)") "Wrong sequence in 'timer_off'. Last: ", &
+                               trim(p%data%name), ". This: ", trim(secname)
+       call die(msg)
     endif
 
-    if (.not. p%active) stop "not active!"
+    if (.not. p%active) then
+       write(msg,"(a,a)") trim(p%data%name), " not active!"
+       call die(msg)
+    endif
     p%active = .false.
     pd => p%data
 
@@ -109,15 +154,14 @@ CONTAINS
     pd%lastTime = deltaTime
     pd%totTime  = pd%totTime + deltaTime
 
-!    print *, "stopping: " // trim(secname)
-
     if (associated(p%caller)) then
        last_active => p%caller
     else
-!       print *, "reached top in off"
+       ! Should not reach this point
     endif
   end subroutine timer_off
 
+  !------------------------------------------------
   subroutine timer_report(secname)
     character(len=*), optional    :: secname
 
@@ -139,58 +183,63 @@ CONTAINS
 
   end subroutine timer_report
 
+  !------------------------------------------------
   recursive subroutine walk_tree(p,level)
-  type(section_t), intent(in),target  :: p
-  integer, intent(in)          :: level
+    type(section_t), intent(in),target  :: p
+    integer, intent(in)          :: level
 
-  integer :: i
-  character(len=40) fmtstr
+    integer :: i
+    character(len=40) fmtstr
 
-  pd => p%data
-  write(fmtstr,"(a,i0,a1,a)") "(", level+1, "x", ",a20,T30,i6,f12.3,f8.2)"
-  write(*,fmtstr) pd%name, pd%nCalls, pd%totTime, 100*pd%totTime/globaltime
-  if (p%nchildren /= 0) then
-     do i=1,p%nchildren
-        call walk_tree(p%child(i),level+1)
-     enddo
-  endif
+    pd => p%data
+    write(fmtstr,"(a,i0,a1,a)") "(", level+1, "x", ",a20,T30,i6,f12.3,f8.2)"
+    write(*,fmtstr) pd%name, pd%nCalls, pd%totTime, 100*pd%totTime/globaltime
+    if (p%nchildren /= 0) then
+       do i=1,p%nchildren
+          call walk_tree(p%child(i),level+1)
+       enddo
+    endif
 
-end subroutine walk_tree
+  end subroutine walk_tree
 
+  !------------------------------------------------
+  subroutine child_index( child, childData, ichild ) 
 
-subroutine child_index( child, childData, ichild )   ! Get index of child in data
+    implicit none
+    character(len=*),intent(in) :: child  ! Child name
+    type(section_t), dimension(:) ,intent(in) :: childData
+    integer,intent(out)                       :: ichild     ! 0 if not found
 
-  implicit none
-  character(len=*),intent(in) :: child  ! Child name
-  type(section_t), dimension(:) ,intent(in) :: childData
-  integer,intent(out)                       :: ichild     ! 0 if not found
+    integer :: kChild, nsize
 
-  integer :: kChild, nsize
+    ichild = 0
+    nsize = size(childData)
 
-  ichild = 0
-  nsize = size(childData)
+    if (nsize>0) then
+       search: do kChild = 1,nsize
+          if (trim(childData(kChild)%data%name) == trim(child)) then
+             iChild = kChild
+             exit search
+          end if
+       end do search
+    end if
 
-  if (nsize>0) then
-    search: do kChild = 1,nsize
-       if (trim(childData(kChild)%data%name) == trim(child)) then
-          iChild = kChild
-          exit search
-       end if
-    end do search
-  end if
+  end subroutine child_index
 
-end subroutine child_index
+  !------------------------------------------------
+  subroutine current_time(t)
+    !
+    ! CPU or walltime, depending on the setting of 'use_walltime'
+    !
+    use m_walltime, only: wall_time
+    real(dp), intent(out) :: t
 
-subroutine current_time(t)
-  use m_walltime, only: wall_time
-  real(dp), intent(out) :: t
-
-  if (use_walltime) then
-     call wall_time(t)
-  else
-     call cpu_time(treal)
-     t = treal
-  endif
-end subroutine current_time
+    if (use_walltime) then
+       call wall_time(t)
+    else
+       call cpu_time(treal)
+       t = treal
+    endif
+  end subroutine current_time
 
 end module m_timer_tree
