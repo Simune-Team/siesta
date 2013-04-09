26a246d672de213a6ed738e9e3e9fd6039457eb0
Jose M Soler <jose.soler@uam.es>
2013-04-09 12:49:59 +0200

    Modified reference to KBM vdW functional (comments only)
    modified:
      Docs/siesta.tex
      Src/SiestaXC/atomxc.F90
      Src/SiestaXC/cellxc.F90
      Src/SiestaXC/siestaxc.F90
      Src/SiestaXC/xcmod.f90

--- Src/SiestaXC/xcmod.f90
+++ Src/SiestaXC/xcmod.f90
@@ -36,7 +36,7 @@
 !           'AM05' => GGA Mattsson & Armiento, PRB, 79, 155101 (2009)
 !          'DRSLL' => VDW Dion et al, PRL 92, 246401 (2004)
 !          'LMKLL' => VDW K.Lee et al, PRB 82, 081101 (2010)
-!            'KBM' => VDW optB88-vdW of J.Klimes et al, JPCM 22, 022201 (2009)
+!            'KBM' => VDW optB88-vdW of J.Klimes et al, JPCM 22, 022201 (2010)
 !             'VV' => VDW Vydrov-VanVoorhis, JCP 133, 244103 (2010)
 !
 ! ------------------------ USAGE ----------------------------------------------
