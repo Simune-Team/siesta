094d630011e96d438fa3c77e211ce685f7090912
Nick Papior Andersen <nicpa@ntch-l0071>
2014-06-03 20:41:30 +0000

    Bugfix for ts_show_regions
    * In case the buffer regions were ending it printed
      an erronous line.
      This has been fixed to handle arbitrary types.
    
    modified:
      Src/ts_show_regions.f90

--- Src/ts_show_regions.f90
+++ Src/ts_show_regions.f90
@@ -1,6 +1,7 @@
 subroutine ts_show_regions(ucell,na_u,xa,N_Elec,Elecs)
   use m_ts_electype
-  use m_ts_method, only : na_Buf, TYP_BUFFER, atom_type
+  use m_ts_method, only : na_Buf, atom_type
+  use m_ts_method, only : TYP_BUFFER, TYP_DEVICE
   use precision, only : dp
   use units, only : Ang
   use parallel, only : IONode
@@ -18,7 +19,6 @@ subroutine ts_show_regions(ucell,na_u,xa,N_Elec,Elecs)
 ! * LOCAL variables  *
 ! ********************
   integer :: ia, i, ia_mid
-  logical :: printed_elec
 
   if ( .not. IONode ) return
 
@@ -26,32 +26,38 @@ subroutine ts_show_regions(ucell,na_u,xa,N_Elec,Elecs)
   ia_mid = (na_u - na_Buf-sum(TotUsedAtoms(Elecs))+1) / 2
 
   write(*,'(/,a)') 'transiesta: Atomic coordinates and regions (Ang):'
-  ia = 0
-  do while ( ia < na_u )
-     if ( atom_type(ia+1) == TYP_BUFFER ) then
-        i = ia + 1
-        do while ( atom_type(i) == TYP_BUFFER )
+  ia = 1
+  do while ( ia <= na_u )
+     select case ( atom_type(ia) )
+     case ( TYP_BUFFER )
+        i = ia
+        do while ( i <= na_u )
+           ! this assures that we do not have a memory leak
+           if ( atom_type(i) /= TYP_BUFFER ) exit
            i = i + 1
         end do
-        call out_REGION(ia,i-ia-1,'Buffer','/')
-     end if
-     printed_elec = .false.
-     do i = 1 , N_Elec
-        if ( ia + 1 == Elecs(i)%idx_na ) then
-           call out_REGION(ia,TotUsedAtoms(Elecs(i)), &
-                trim(name(Elecs(i)))//' electrode','#')
-           printed_elec = .true.
-        end if
-     end do
-     if ( .not. printed_elec ) then
-        ia = ia + 1
+        i = i - ia
+        ! steps ia counter
+        call out_REGION(ia,i,'Buffer','/')
+     case ( TYP_DEVICE )
         ia_mid = ia_mid - 1
         if ( ia_mid == 0 ) then
            write(*,'(tr1,3(tr2,f12.7),tr8,a)') xa(:,ia)/Ang,'Device'
         else
            write(*,'(tr1,3(tr2,f12.7))') xa(:,ia)/Ang
         end if
-     end if
+        ia = ia + 1
+     case default
+        ! electrode position
+        do i = 1 , N_Elec
+           if ( ia == Elecs(i)%idx_na ) then
+              ! steps ia counter
+              call out_REGION(ia,TotUsedAtoms(Elecs(i)), &
+                   trim(name(Elecs(i)))//' electrode','#')
+              exit
+           end if
+        end do
+     end select
   end do
 
   write(*,*)
@@ -69,7 +75,6 @@ contains
     mid = (NA+1) / 2
     write(*,'(a)') repeat(marker,46)
     do i = 1, NA
-       ia = ia + 1
        if ( i == mid ) then
           write(*,'(a1,3(tr2,f12.7),tr2,a1,tr5,a)') &
                marker,xa(:,ia)/Ang,marker,trim(name)
@@ -77,6 +82,7 @@ contains
           write(*,'(a1,3(tr2,f12.7),tr2,a1)') &
                marker,xa(:,ia)/Ang,marker
        end if
+       ia = ia + 1
     end do
     write(*,'(a)') repeat(marker,46)
 
