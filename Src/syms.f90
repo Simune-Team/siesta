a528fe91e1ac418466e0d0e625cee387dbf9c6ce
mverstra <mverstra@wellington>
2014-06-13 15:45:15 +0200

    added xa and fa symmetrizers in constr.f90 + helper routines in syms.f90

--- Src/syms.f90
+++ Src/syms.f90
@@ -8,6 +8,7 @@
 ! Use of this software constitutes agreement with the full conditions
 ! given in the SIESTA license, as signed by all legitimate users.
 !
+! written by Matthieu Verstraete around 12 June 2014
 ! 
 
 module m_syms
@@ -19,7 +20,10 @@ module m_syms
     integer :: space_group
     character (len=11) :: symbol
     integer, allocatable :: symops(:,:,:)
+    integer, allocatable :: symops_cart(:,:,:)
+    integer, allocatable :: symops_recip(:,:,:)
     double precision, allocatable :: trans(:,:)
+    double precision, allocatable :: trans_cart(:,:)
   end type syms_type
 
   ! for the moment this single instance of the syms is exported by the module.
@@ -87,13 +91,15 @@ contains
     implicit         none
     type(syms_type), intent(out) :: syms_this
     integer, intent(in)          :: na, isa(na)
-    double precision, intent(in) :: cell(3,3), xa(3,na)
+    double precision, intent(inout) :: cell(3,3), xa(3,na)
 
 ! local vars
     integer :: max_size
     integer :: ii, info
     double precision :: symprec
     double precision :: cellinv(3,3)
+    double precision :: dsymop(3,3)
+    double precision :: dsymopinv(3,3)
 
     double precision, allocatable :: xreda(:,:)
     integer, allocatable :: symops_tmp(:,:,:)
@@ -101,12 +107,19 @@ contains
 
     symprec= 1.e-6 ! this is hard coded for the moment - could be input var for init_syms
 
-    call INVER(cell,cellinv,3,3,info)
-    if (info .ne. 0) stop 'subroutine constr: error in inverse matrix of cell'
-
     allocate (xreda(3,na))
-    ! might have to check if the cellinv needs to be transposed here...
-    xreda = matmul(cellinv, xa)
+    call cart2red(cell, na, xa, xreda)
+print *,  'xred = '
+print '(3E20.10)', xreda
+
+    ! refines positions with highest symmetry found within symprec
+    ! removed 13/6/2014 - this returns the conventional cell!!!
+    !call spg_refine_cell(cell, xreda, isa, na, symprec)
+    !print *, 'xred refined'
+    !print '(3E20.10)', xreda
+    !print *, 'cell refined'
+    !print '(3E20.10)', cell
+
     call spg_get_international(syms_this%space_group, syms_this%symbol, cell(1,1), xreda(1,1), isa(1), na, symprec)
 
     call spg_get_multiplicity(max_size, cell(1,1), xreda(1,1), isa(1), na, symprec)
@@ -121,14 +134,52 @@ contains
 
     allocate(syms_this%symops(1:3, 1:3, 1:syms_this%nsymop))
     do ii = 1, max_size
-      syms_this%symops(:,:,ii) = transpose(symops_tmp(:,:,ii)) ! transpose needed or not? only visible on triclinic or hexagonal etc..
+! transpose needed or not? only visible on triclinic or hexagonal etc..
+! Checked for 1 case with Rm-3m comparing to abinit - no transpose needed...
+      syms_this%symops(:,:,ii) = symops_tmp(:,:,ii)
     end do
     deallocate(symops_tmp)
+
+! get symops in cartesian coordinates - should still be integer -1 0 1 elements
+    allocate(syms_this%symops_cart(1:3, 1:3, 1:syms_this%nsymop))
+    call INVER(cell,cellinv,3,3,info)
+    if (info .ne. 0) stop 'subroutine init_syms: error in inverse matrix of cell'
+
+    do ii = 1, syms_this%nsymop
+      syms_this%symops_cart(:, :, ii) = nint( matmul(cell, &
+&       matmul(syms_this%symops(:, :, ii), cellinv)) )
+
+! DEBUG - comment out later
+  if (any(abs(dble(syms_this%symops_cart(:, :, ii)) - matmul(cell, &
+&       matmul(syms_this%symops(:, :, ii), cellinv))) > 1.e-10)) then
+    stop 'error : cartesian symop element is not -1 0 +1'
+  end if
+! END DEBUG
+
+    end do
+
+! get symops in reciprocal space = normal^-1 ^T
+    allocate(syms_this%symops_recip(1:3, 1:3, 1:syms_this%nsymop))
+    do ii = 1, syms_this%nsymop
+      dsymop = dble(syms_this%symops(:, :, ii)) 
+      call INVER(dsymop,dsymopinv,3,3,info)
+      if (info .ne. 0) stop 'subroutine init_syms: error in inverse matrix of symop'
+      dsymopinv = transpose (dsymopinv)
+      syms_this%symops_recip(:,:,ii) = int(dsymopinv)
+! DEBUG - comment out later
+  if (any(abs(dble(syms_this%symops_recip(:, :, ii)) - dsymopinv(:,:)) > 1.e-10)) then
+    stop 'error : recip symop element is not -1 0 +1'
+  end if
+! END DEBUG
+    end do
 
     allocate(syms_this%trans(1:3, 1:syms_this%nsymop))
     syms_this%trans = trans_tmp(:,1:syms_this%nsymop)
     deallocate(trans_tmp)
     
+    allocate(syms_this%trans_cart(1:3, 1:syms_this%nsymop))
+    syms_this%trans_cart = matmul(cell, syms_this%trans)
+
   end subroutine init_syms
 
   subroutine delete_syms( syms_this )
@@ -140,9 +191,12 @@ contains
     type(syms_type), intent(inout) :: syms_this
 
     if(allocated(syms_this%symops)) deallocate (syms_this%symops)
+    if(allocated(syms_this%symops_cart)) deallocate (syms_this%symops_cart)
     if(allocated(syms_this%trans)) deallocate (syms_this%trans)
+    if(allocated(syms_this%trans_cart)) deallocate (syms_this%trans_cart)
 
   end subroutine delete_syms
+
 
   subroutine print_syms( syms_this )
 ! *****************************************************************
@@ -159,15 +213,86 @@ contains
     write (*,'(a)') " output of crystal symmetries "
     write (*,'(a,I6)') " nsymop = ", syms_this%nsymop
     write (*,'(2a)') " symbol = ", syms_this%symbol
-    write (*,'(a)') " symops = "
+    write (*,'(a)') " symops in reduced coordinates = "
     do ii = 1, syms_this%nsymop
       write (*,'(3(3I10,2x))') syms_this%symops(:,:,ii)
     end do
-    write (*,'(a)') " trans = "
+    write (*,'(a)') " trans in reduced coordinates = "
     do ii = 1, syms_this%nsymop
       write (*, '(3(E20.10,2x))') syms_this%trans(:,ii)
     end do
   end subroutine print_syms
 
+
+  subroutine red2cart(cell, na, xred, xa)
+! *****************************************************************
+! Arguments:
+! cell : unit cell vectors
+! na : number of atoms
+! xa : cartesian position vectors
+! xred : reduced position vectors
+! *****************************************************************
+    implicit         none
+    double precision,intent(in) :: cell(3,3)
+    integer,intent(in) :: na
+    double precision,intent(in) :: xred(3,na)
+    double precision,intent(out) :: xa(3,na)
+
+    xa = matmul(cell, xred)
+
+  end subroutine red2cart
+
+
+  subroutine cart2red(cell, na, xa, xred)
+! *****************************************************************
+! Arguments:
+! cell : unit cell vectors
+! na : number of atoms
+! xa : cartesian position vectors
+! xred : reduced position vectors
+! *****************************************************************
+    implicit         none
+    double precision,intent(in) :: cell(3,3)
+    integer,intent(in) :: na
+    double precision,intent(in) :: xa(3,na)
+    double precision,intent(out) :: xred(3,na)
+
+    double precision :: cellinv(3,3)
+    integer :: info
+
+    call INVER(cell,cellinv,3,3,info)
+    if (info .ne. 0) stop 'subroutine cart2red: error in inverse matrix of cell'
+
+    xred = matmul(cellinv, xa)
+
+  end subroutine cart2red
+
+  function wrapvec_zero_one(num) result (red)
+    implicit none
+    double precision, intent(in) :: num(3)
+    double precision :: red(3)
+
+    integer :: ii
+
+    do ii=1, 3
+      red(ii) = wrap2zero_one(num(ii))
+    end do
+
+  end function wrapvec_zero_one
+
+  function wrap2zero_one(num) result (red)
+    implicit none
+    double precision, intent(in) :: num
+    double precision :: red
+
+    if (num>0.0d0) then
+      red=mod((num+1.d-13),1.0d0)-1.d-13
+    else
+      red=-mod(-(num-1.0d0+1.d-13),1.0d0)+1.0d0-1.d-13
+    end if
+    if(abs(red)<1.d-13)red=0.0d0
+
+  end function wrap2zero_one
+   
 
 end module m_syms
