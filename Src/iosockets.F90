31b4364ed8e827e3e46a48e42f1cc64036f5fdc5
Jose M Soler <jose.soler@uam.es>
2015-03-13 12:11:57 +0100

    iosockets.F90 adapted to siesta style
    modified:
      Src/iosockets.F90

--- Src/iosockets.F90
+++ Src/iosockets.F90
@@ -1,55 +1,93 @@
-! 
-! This file is part of the SIESTA package.
-!
-! Copyright (c) Fundacion General Universidad Autonoma de Madrid:
-! E.Artacho, J.Gale, A.Garcia, J.Junquera, P.Ordejon, D.Sanchez-Portal
-! and J.M.Soler, 1996- .
-! 
-! Use of this software constitutes agreement with the full conditions
-! given in the SIESTA license, as signed by all legitimate users.
-!
-module iosockets
+!!@LICENSE
+
+!*****************************************************************************
+! module iosockets
+!*****************************************************************************
 ! Handles communications between siesta-as-a-subroutine and a driver 
 ! program, transfering coordinates and forces trough POSIX sockets.
 ! The routines that handle the other side of the communication are
-! in module fsiesta.
-! modified from iopipes J.M.Soler and A.Garcia. Nov.2003
-! M. Ceriotti 2014
+! in file fsiesta_sockets.f90 and/or in the i-PI code.
+! J.M.Soler, A.Garcia, and M.Ceriotti, 2014
+!*****************************************************************************
+!
+!   Modules used:
+! use precision,    only: dp
+! use parallel,     only: IOnode
+! use fdf
+! use m_fdf_global, only: fdf_global_get
+! use f90sockets,   only: open_socket, writebuffer, readbuffer
+! use sys,          only: die, bye
+! use m_mpi_utils,  only: broadcast
+! use cellSubs,     only: volcel
+! use mpi_siesta
+!
+!   Public derived types:
+! none
+!
+!   Public variables and arrays:
+! none
+!
+!   Public procedures:
+! coordsFromSocket, &! receive coords and cell vectors from master program
+! forcesToSocket     ! send energy, forces, and stress to master program
+!
+!*****************************************************************************
+! subroutine coordsFromSocket( na, xa, cell )
+!   integer,  intent(in) :: na            ! Number of atoms
+!   real(dp), intent(out):: xa(3,na)      ! Atomic coordinates (bohr)
+!   real(dp), intent(out):: cell(3,3)     ! Lattice vectors (bohr)
+!*****************************************************************************
+! subroutine forcesToSocket( na, energy, forces, stress )
+!   integer,  intent(in) :: na            ! Number of atoms
+!   real(dp), intent(in) :: energy        ! Total energy (Ry)
+!   real(dp), intent(in) :: forces(3,na)  ! Atomic forces (Ry/bohr)
+!   real(dp), intent(in) :: stress(3,3)   ! Stress tensor (Ry/bohr^3)
+!*****************************************************************************
 
-use precision, only: dp
-use parallel, only: IOnode
-use fdf
-use m_fdf_global, only: fdf_global_get
-use f90sockets, ONLY : open_socket, writebuffer, readbuffer
-use sys, only: die, bye
-use  m_mpi_utils, only: broadcast
+module iosockets
+
+! Used modules
+  use precision,    only: dp
+  use parallel,     only: IOnode
+  use fdf
+  use m_fdf_global, only: fdf_global_get
+  use f90sockets,   only: open_socket, writebuffer, readbuffer
+  use sys,          only: die, bye
+  use m_mpi_utils,  only: broadcast
+  use cellSubs,     only: volcel
 #ifdef MPI
-      use mpi_siesta
+  use mpi_siesta
 #endif
 
   implicit none
 
-  external :: io_assign, io_close
-
-PUBLIC :: coordsFromSocket, forcesToSocket
+! Public procedures
+PUBLIC :: &
+  coordsFromSocket, &! receive coords and cell vectors from master program
+  forcesToSocket     ! send energy, forces, and stress to master program
 
 PRIVATE  ! Nothing is declared public beyond this point
 
-  character(len=*), parameter :: siesta_xunit = 'Bohr'
-  character(len=*), parameter :: siesta_eunit = 'Ry'
-  character(len=*), parameter :: siesta_funit = 'Ry/Bohr'
-  character(len=*), parameter :: siesta_sunit = 'Ry/Bohr**3'
+! Private module parameters
+  integer,          parameter :: unit_len = 12
+  character(len=*), parameter :: siesta_xunit = 'bohr'
+  character(len=*), parameter :: siesta_eunit = 'ry'
+  character(len=*), parameter ::    ipi_xunit = 'bohr'
+  character(len=*), parameter ::    ipi_eunit = 'hartree'
 
-  INTEGER, SAVE :: socket, nat
-  REAL*8, ALLOCATABLE,SAVE :: combuf(:), cellv
-
-  integer,           save :: iuc, iuf
-
+! Private module variables
+  integer, save :: socket
+  real(dp),save :: cellv
+  character(len=unit_len),save:: master='unknown'
+  character(len=32),      save:: master_eunit='unknown', &
+                                 master_xunit='unknown'
 #ifdef MPI
   integer :: MPIerror
 #endif
 
 CONTAINS
+
+!*****************************************************************************
 
 subroutine coordsFromSocket( na, xa, cell )
   ! Reads coordinates from socket
@@ -58,90 +96,112 @@ subroutine coordsFromSocket( na, xa, cell )
   real(dp), intent(out) :: xa(3,na)   ! Atomic coordinates (bohr)
   real(dp), intent(out) :: cell(3,3)  ! Lattice vectors (bohr)
 
-  logical, save     :: firstTime = .true.
-
+! Local parameters
+  character(len=*),parameter:: myName='coordsFromSocket '
   INTEGER, PARAMETER :: MSGLEN=12
-  LOGICAL :: isinit=.false., hasdata=.false., firststep=.true., isunix
-  CHARACTER*12 :: header
-  CHARACTER*1024 :: parbuffer, host
-  INTEGER ::  inet, port, i
-  REAL *8 :: cellh(3,3), cellih(3,3), mtxbuf(9)
 
-  ! Open the socket -- this will be shared from the receive and the send side of communication
+! Local variables and arrays
+  logical, save  :: firstTime = .true.
+  LOGICAL        :: isunix
+  CHARACTER*12   :: code, header
+  CHARACTER*1024 :: parbuffer, host
+  INTEGER        :: inet, port, n
+  REAL*8         :: aux(9), c(9)
+  REAL*8,ALLOCATABLE:: x(:)
+
+! Open the socket, shared by the receive and send sides of communication
   if (firstTime) then
-    write(*,*) " Opening socket fot two-way communication. "
-    call fdf_global_get(host, "Master.address", "localhost")
-    call fdf_global_get(port, "Master.port", 31415)
+    call fdf_global_get(master, "Master.code", "fsiesta")
+    call fdf_global_get(host,   "Master.address", "localhost")
+    call fdf_global_get(port,   "Master.port", 31415)
     call fdf_global_get(isunix, "Master.unix", .false.)
 
+    inet = 1
     if (isunix) inet=0
     host = TRIM(host)//achar(0)
 
-    if (IOnode) call open_socket(socket, inet, port, host) 
+    if (IOnode) then
+      print*,myName//"Opening socket fot two-way communication."
+      call open_socket(socket, inet, port, host)
+    endif
 
     firstTime = .false.
   end if ! (firstTime .and. IOnode)
-  header=""
 
-! Read coordinates from pipe
+! Read header from socket
+  header=""
   if (IOnode) then
-    write(*,*) " Receiving coordinates from socket. "
+    print'(/,a)',myName//"Receiving coordinates from socket. "
     do
       call readbuffer(socket, header, MSGLEN)
-      if (trim(header)/='STATUS') exit
-      call writebuffer(socket,"READY       ",MSGLEN)      
+      if (trim(master)=='ipi') then
+        if (trim(header)/='STATUS') exit 
+        call writebuffer(socket,"READY       ",MSGLEN)
+      elseif (trim(master)=='fsiesta') then
+        if (trim(header)/='wait') exit
+        call writebuffer(socket,"ready       ",MSGLEN)
+      endif ! trim(master)
     enddo
+  endif ! IOnode
+#ifdef MPI
+  call MPI_Bcast(header, 12, MPI_Character, 0, MPI_Comm_World, MPIerror)
+#endif
+
+! Read cell vectors from socket, as a single buffer vector
+  if (IOnode) then
+    if (master=="i-pi" .and. trim(header)=="POSDATA") then 
+      call readbuffer(socket, c, 9)
+      call readbuffer(socket, aux, 9)    ! not used in siesta
+      master_xunit = ipi_xunit
+      master_eunit = ipi_eunit
+    elseif (master=="fsiesta" .and. trim(header)=="begin_coords") then
+      call readbuffer(socket, master_xunit, unit_len)
+      call readbuffer(socket, master_eunit, unit_len)
+      call readbuffer(socket, c, 9)
+    else
+      call die(myName//'Unexpected message from master')
+    end if
   endif
 
-#ifdef MPI !broadcasts the message we just received
-  call MPI_Bcast(header, 12, MPI_Character, 0,  &
-                 MPI_Comm_World, MPIerror)
-#endif
-
-  if (trim(header) == "POSDATA") then 
-    !receives the positions & the cell data
-    ! first reads cell and the number of atoms
-    if (ionode) call readbuffer(socket, mtxbuf, 9)
-    cellh = RESHAPE(mtxbuf,(/3,3/))
-    if (ionode) call readbuffer(socket, mtxbuf, 9)
-    cellih = RESHAPE(mtxbuf,(/3,3/))
-    if (ionode) call readbuffer(socket, nat)
-  
+! Broadcast and copy cell from buffer
 #ifdef MPI
-    call MPI_Bcast(cellh(1,1),9, MPI_Double_Precision,0,  &
-            MPI_Comm_World, MPIerror)
-    call MPI_Bcast(cellih(1,1),9, MPI_Double_Precision,0,  &
-            MPI_Comm_World, MPIerror)
-    call MPI_Bcast(nat,1, MPI_Integer,0,  &
-            MPI_Comm_World, MPIerror)
-#endif
-    if (.not.allocated(combuf)) then
-      allocate(combuf(3*nat))
-    end if
-    if (ionode) call readbuffer(socket, combuf, nat*3)
-
-#ifdef MPI
-    call MPI_Bcast(combuf,3*nat, MPI_Double_Precision,0,  &
+  call MPI_Bcast(master_xunit, unit_len, MPI_Character,0,  &
                  MPI_Comm_World, MPIerror)
+  call MPI_Bcast(master_eunit, unit_len, MPI_Character,0,  &
+                 MPI_Comm_World, MPIerror)
+  call MPI_Bcast(c,9, MPI_Double_Precision,0, MPI_Comm_World, MPIerror)
 #endif
+  cell = RESHAPE( c, (/3,3/) )
 
-   print '(/,3a,/,(3f12.6))', &
-      'coordsFromSocket: cell (',trim('Bohr'),') =', cellh
-   print '(  3a,/,(3f12.6))', &
-      'coordsFromSocket: xa (',trim('Bohr'),') =', xa
-   ! Convert coordinate units         
-    cell = cellh * fdf_convfac( 'Bohr', siesta_xunit )
-    xa   = RESHAPE(combuf, (/ 3 , nat /) )  &
-           * fdf_convfac( 'Bohr', siesta_xunit )
-   ! Computes the volume of the cell to be used to scale the stress tensor
-    cellv = ABS(cell(1,1) * cell(2,2) * cell(3,3)) ! the cell is upper triangular, so super easy!
-    write(*,*) "CELL VOLUME", cellv  
- else
-   call die('coordsFromSocket: Unexpected message from server')
- end if
+! Read and check number of atoms
+  if (ionode) then
+    call readbuffer(socket, n)
+    if (n/=na) call die(myName//'ERROR: unexpected number of atoms')
+  endif
+
+! Read atomic coordinates
+  allocate(x(3*na))
+  if (ionode) call readbuffer(socket, x, 3*na)
+#ifdef MPI
+  call MPI_Bcast(x,3*na, MPI_Double_Precision,0, MPI_Comm_World, MPIerror)
+#endif
+  xa = RESHAPE( x, (/3,na/) )
+  deallocate(x)
+
+! Print coordinates and cell vectors received
+  print '(/,4a,/,(3f12.6))', myName,'cell (',trim(master_xunit),') =', cell
+  print '(  4a,/,(3f12.6))', myName,'coords (',trim(master_xunit),') =', xa
+
+! Convert physical units
+  cell = cell * fdf_convfac( master_xunit, siesta_xunit )
+  xa   = xa   * fdf_convfac( master_xunit, siesta_xunit )
+
+! Compute and save the cell volume, to be used by forcesToSocket
+  cellv = volcel(cell)
    
 end subroutine coordsFromSocket
 
+!*****************************************************************************
 
 subroutine forcesToSocket( na, energy, forces, stress )
 ! Writes stress and forces to socket
@@ -151,42 +211,78 @@ subroutine forcesToSocket( na, energy, forces, stress )
   real(dp), intent(in) :: forces(3,na)  ! Atomic forces (Ry/bohr)
   real(dp), intent(in) :: stress(3,3)   ! Stress tensor (Ry/bohr^3)
 
-  integer           :: i, ia
-  real*8   :: pot, vir(3,3)
+! Local parameters
+  character(len=*),parameter:: myName='forcesToSocket '
   INTEGER, PARAMETER :: MSGLEN=12
-  CHARACTER*12 :: header
 
-  if (IOnode) write(*,*) " Sending forces to socket. "
+! Local variables and arrays
+  CHARACTER*12      :: header
+  real*8            :: e, s(9), vir(9)
+  real*8,allocatable:: f(:)
 
+! Copy input to local variables
+  allocate(f(3*na))
+  e = energy
+  f = reshape( forces, (/3*na/) )
+  s = reshape( stress, (/9/) )
+  
 ! Convert physical units
-  pot = energy * fdf_convfac( siesta_eunit, 'Ry' ) * 0.5 ! i-PI wants Hartree!
-  ! we want to return the potential energy virial term, *without* the volume scaling
-  ! also i-PI and Siesta use different sign conventions for the virial, so we need to cope with that.
-  vir =  -1.0 * stress * cellv * fdf_convfac( siesta_eunit, 'Ry' ) * 0.5 
-  combuf = RESHAPE(forces, (/ 3 * nat /) ) * fdf_convfac( siesta_funit, 'Ry/Bohr' ) * 0.5
+  e = e * fdf_convfac( siesta_eunit, master_eunit )
+  f = f * fdf_convfac( siesta_eunit, master_eunit ) &
+        / fdf_convfac( siesta_xunit, master_xunit )
+  s = s * fdf_convfac( siesta_eunit, master_eunit ) &
+        / fdf_convfac( siesta_xunit, master_xunit )**3
+
+! Find virial tensor for i-pi, in master's units
+  vir = -s * cellv * fdf_convfac( siesta_xunit, master_xunit )**3
 
 ! Write forces to socket
   if (IOnode) then
-    do
-      call readbuffer(socket, header, MSGLEN)
-      if (trim(header)/='STATUS') exit
-      call writebuffer(socket,"HAVEDATA    ",MSGLEN)
-    enddo
-    if (trim(header)/='GETFORCE') &
-        call die('forcesToSocket: Error in socket communication!') 
-    call writebuffer(socket,"FORCEREADY  ",MSGLEN)
-    call writebuffer(socket,pot)
-    call writebuffer(socket,nat)
-    call writebuffer(socket,combuf,3*nat)
-    call writebuffer(socket,reshape(vir,(/9/)),9)
-          
-    ! i-pi can also receive an arbitrary string, that will be printed out to the "extra" 
-    ! trajectory file. this is useful if you want to return additional information, e.g.
-    ! atomic charges, wannier centres, etc. one must return the number of characters, then
-    ! the string. here we just send back zero characters.
-    nat = 0
-    call writebuffer(socket,nat)
+    print'(/,a)',myName//"Sending forces to socket. "
+    if (trim(master)=='i-pi') then
+      do
+        call readbuffer(socket, header, MSGLEN)
+        if (trim(header)/='STATUS') exit
+        call writebuffer(socket,"HAVEDATA    ",MSGLEN)
+      enddo
+      if (trim(header)/='GETFORCE') &
+          call die(myName//'ERROR in socket communication!') 
+      call writebuffer(socket,"FORCEREADY  ",MSGLEN)
+      call writebuffer(socket,e)
+      call writebuffer(socket,na)
+      call writebuffer(socket,f,3*na)
+      call writebuffer(socket,vir,9)       
+      ! i-pi can also receive an arbitrary string, that will be printed 
+      ! out to the "extra" trajectory file. This is useful if you want 
+      ! to return additional information, e.g. atomic charges, wannier 
+      ! centres, etc. one must return the number of characters, then
+      ! the string. here we just send back zero characters.
+      call writebuffer(socket,0)
+!      call writebuffer(socket,'')  ! would this not be clearer?
+    elseif (trim(master)=='fsiesta') then
+      do
+        call readbuffer(socket, header, MSGLEN)
+        if (trim(header)/='wait') exit
+      enddo
+      call writebuffer(socket,'begin_forces',12)
+      call writebuffer(socket,e)
+      call writebuffer(socket,s,9)
+      call writebuffer(socket,na)
+      call writebuffer(socket,f,3*na)
+      call writebuffer(socket,'end_forces',10)
+    else
+      call die(myName//'ERROR: unknown master')
+    endif ! trim(master)
   end if ! IOnode
+
+! Print energy, forces, and stress tensor sent to master
+  print '(/,a,f12.6)',      myName// &
+    'energy ('//trim(master_eunit)//') =', e
+  print '(  a,/,(3f12.6))', myName// &
+    'forces ('//trim(master_eunit)//'/'//trim(master_xunit)//') =', f
+  print '(  a,/,(3f12.6))', myName// &
+    'stress ('//trim(master_eunit)//'/'//trim(master_xunit)//'^3) =', s
+  deallocate(f)
 
 end subroutine forcesToSocket
 
