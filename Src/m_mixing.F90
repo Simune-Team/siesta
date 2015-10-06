68585d9aa50343f5a5ba8aa7df6b370618775506
Nick Papior Andersen <nicpa@ntch-l0071>
2015-10-06 19:57:22 +0000

    Bugfix for transition from diagon to transiesta using GR
    * When going from diagon to transiesta with GR mixing
      the counter was not reset which meant that it would
      crash for any convergence on an even history.
    
    * Ensured that the Fstack always returns an
      integer when trying to determine its size (it defaults
      to 0)
    
    added:
      Src/m_mixing_scf.F90
    modified:
      Src/Fstack.T90
      Src/Makefile
      Src/m_mixing.F90
      Src/siesta_forces.F

--- Src/m_mixing.F90
+++ Src/m_mixing.F90
@@ -949,10 +949,10 @@ contains
 
        ! Add the residual to the stack
        call push_F(mix%stack(1), n, F1, mix%rv(1))
-       
+
        ! The history in this scheme is a little obscure... :)
        if ( mod(mix%cur_itt,2) == 1 ) then
-
+          
           if ( debug_mix ) &
                write(*,'(2a)') trim(debug_msg), &
                ' Direct mixing'
@@ -1955,76 +1955,4 @@ contains
   end subroutine push_diff
 
 end module m_mixing
-
-
-! Also the mixing container
-module m_mixing_scf
-
-  use class_Fstack_dData1D
-  use m_mixing, only: tMixer
-  use m_mixing, only: mixing_reset, mixing_history_clear
-
-  implicit none
-
-  private
-  save
-
-  type(tMixer), pointer :: scf_mixs(:) => null()
-  type(tMixer), pointer :: scf_mix => null()
-
-  public :: scf_mixs, scf_mix
-
-  public :: reset_mixing_scf
-  public :: mixing_scf_converged
-  public :: mixing_scf_history_clear
-
-contains
-
-  subroutine mixing_scf_converged( SCFconverged )
-    use parallel, only: IONode
-    logical, intent(inout) :: SCFconverged
-    integer :: i
-
-    ! Return if no convergence
-    if ( .not. SCFconverged ) return
-
-    if ( scf_mix%n_itt < 0 ) then
-       ! this means that we skip to the 
-       ! following algorithm
-       scf_mix => scf_mixs(-scf_mix%n_itt)
-       SCFconverged = .false.
-
-       if ( allocated(scf_mix%stack) ) then
-
-          do i = 1 , size(scf_mix%stack)
-             ! delete all but one history
-             ! This should be fine
-             call reset(scf_mix%stack(i), -1)
-          end do
-
-       end if
-
-       if ( IONode ) then
-         write(*,'(a)') ':!: SCF cycle continued for different mixer'
-       end if
-
-    end if
-
-  end subroutine mixing_scf_converged
-
-  subroutine reset_mixing_scf()
-
-    nullify(scf_mix)
-    call mixing_reset( scf_mixs )
-
-  end subroutine reset_mixing_scf
-
-  subroutine mixing_scf_history_clear( )
-    
-    call mixing_history_clear( scf_mixs )
-    scf_mix => scf_mixs(1)
-    
-  end subroutine mixing_scf_history_clear
-
-end module m_mixing_scf
   
---
+++ Src/m_mixing_scf.F90
@@ -0,0 +1,70 @@
+! Also the mixing container
+module m_mixing_scf
+
+  use class_Fstack_dData1D
+  use m_mixing, only: tMixer
+  use m_mixing, only: mixing_reset, mixing_history_clear
+
+  implicit none
+
+  private
+  save
+
+  type(tMixer), pointer :: scf_mixs(:) => null()
+  type(tMixer), pointer :: scf_mix => null()
+
+  public :: scf_mixs, scf_mix
+
+  public :: reset_mixing_scf
+  public :: mixing_scf_converged
+  public :: mixing_scf_history_clear
+
+contains
+
+  subroutine mixing_scf_converged( SCFconverged )
+    use parallel, only: IONode
+    logical, intent(inout) :: SCFconverged
+    integer :: i
+
+    ! Return if no convergence
+    if ( .not. SCFconverged ) return
+
+    if ( scf_mix%n_itt < 0 ) then
+       ! this means that we skip to the 
+       ! following algorithm
+       scf_mix => scf_mixs(-scf_mix%n_itt)
+       SCFconverged = .false.
+
+       if ( allocated(scf_mix%stack) ) then
+
+          do i = 1 , size(scf_mix%stack)
+             ! delete all but one history
+             ! This should be fine
+             call reset(scf_mix%stack(i), -1)
+          end do
+
+       end if
+
+       if ( IONode ) then
+         write(*,'(a)') ':!: SCF cycle continued for different mixer'
+       end if
+
+    end if
+
+  end subroutine mixing_scf_converged
+
+  subroutine reset_mixing_scf()
+
+    nullify(scf_mix)
+    call mixing_reset( scf_mixs )
+
+  end subroutine reset_mixing_scf
+
+  subroutine mixing_scf_history_clear( )
+    
+    call mixing_history_clear( scf_mixs )
+    scf_mix => scf_mixs(1)
+    
+  end subroutine mixing_scf_history_clear
+
+end module m_mixing_scf
