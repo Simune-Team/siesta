52f09a0d5b72c5b4b2d1c60e86fde82dcb8545e5
Alberto Garcia <albertog@icmab.es>
2016-06-29 05:38:48 +0200

    Remove Pseudo/atom

--- Pseudo/atom/corecorr.h
+++
@@ -1,9 +0,0 @@
-c------
-c     Core-correction data
-c
-      double precision rc_core
-      integer          n_of_continuous_derivs
-c
-      common /corecorr/ rc_core, n_of_continuous_derivs
-      save /corecorr/
-c------
