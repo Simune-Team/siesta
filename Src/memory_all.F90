5c1219f81ad876ac5ffe027f9b55bce5b9a56e44
Nick Papior <nickpapior@gmail.com>
2016-08-10 13:29:54 +0200

    Removed superfluous print-outs
    - The memory print-outs was more than likely related
      to PEXSI for checking that the splitting of the workers
      and PEXSI-nodes
    
    - The entropy print-out is now only performed in PEXSI compilations
    
    - The defaults for the timers are now reverted in case of non-PEXSI
      runs.
    
    - Updated the manual for a couple of entries related to timings.

--- Src/memory_all.F90
+++ Src/memory_all.F90
@@ -1,35 +1,33 @@
 subroutine memory_all(str,comm)
- use m_rusage, only :  rss_max
+  use m_rusage, only :  rss_max
 #ifdef MPI  
- use mpi
+  use mpi
 #endif
- character(len=*), intent(in) :: str
- integer, intent(in)          :: comm
+  character(len=*), intent(in) :: str
+  integer, intent(in)          :: comm
 
- integer :: mpierror
- real    :: max_mem, min_mem, my_mem
- integer :: nprocs, myrank
+  integer :: mpierror
+  real    :: mem(2), tmem(2)
+  integer :: nprocs, myrank
 
 #ifdef MPI
-    call MPI_Comm_Size( Comm, nprocs, MPIerror )
-    call MPI_Comm_Rank( Comm, myrank, MPIerror )
+  call MPI_Comm_Size( Comm, nprocs, MPIerror )
+  call MPI_Comm_Rank( Comm, myrank, MPIerror )
 #else
-    nprocs = 1
-    myrank = 0
+  nprocs = 1
+  myrank = 0
 #endif
 
- my_mem = rss_max()
+  mem(2) = rss_max()
+  mem(1) = - mem(2)
 
-#ifdef MPI 
- call MPI_Reduce(my_mem,max_mem,1,MPI_Real,MPI_max,0,comm,MPIerror)
- call MPI_Reduce(my_mem,min_mem,1,MPI_Real,MPI_min,0,comm,MPIerror)
-#else
- max_mem = my_mem
- min_mem = my_mem
+#ifdef MPI
+  call MPI_Reduce(mem,tmem,2,MPI_Real,MPI_max,0,comm,MPIerror)
+  mem = tmem
 #endif
- 
- if (myrank == 0) then
-    write(6,"(a,2f12.2)") " &m -- Peak memory (Mb) " // trim(str) // " (max,min): ",  max_mem, min_mem
- endif
+
+  if (myrank == 0) then
+     write(6,"(a,2f12.2)") " &m -- Peak memory (Mb) " // trim(str) // " (max,min): ",  mem(2), -mem(1)
+  endif
 
 end subroutine memory_all
