162688b5b92f45989034c1bf5c75b4fd8b1594f2
Alberto Garcia <albertog@icmab.es>
2019-03-26 17:52:01 +0100

    Hermitify dscf in print_spin

--- Src/print_spin.F90
+++ Src/print_spin.F90
@@ -2,10 +2,10 @@ subroutine print_spin(qspin)
   !
   ! Prints spin in output and CML files
   !
-  use m_spin,          only: nspin
+  use m_spin,          only: nspin  ! This is spin%grid (1, 2, or 4)
   use atomlist,        only: no_l
   use sparse_matrices, only: listhptr, numh
-  use sparse_matrices, only: S, Dscf
+  use sparse_matrices, only: S, Dscf   ! Dscf could have 1, 2, 4, or 8 components
   use siesta_cml
   use parallel,        only: IOnode
   use precision,       only: dp
@@ -21,11 +21,31 @@ subroutine print_spin(qspin)
   real(dp) :: qaux
   real(dp) :: Stot        ! Total spin magnitude
   real(dp) :: Svec(3)     ! Total spin vector
+  real(dp) :: dm_aux(4)   ! Auxiliary array to hermitify Dscf for SOC
 #ifdef MPI
   real(dp) :: qtmp(4)
 #endif
 
-  if (nspin .ge. 2) then
+  if (nspin < 2) RETURN
+
+  if (spin%SO) then
+     
+     do ispin = 1,nspin
+        qspin(ispin) = 0.0_dp
+        do io = 1,no_l
+           do j = 1,numh(io)
+              ind = listhptr(io)+j
+              ! In the SOC case, hermitify Dscf
+              dm_aux(1:4) = dscf(ind,1:4)
+              dm_aux(3) = 0.5_dp*(dm_aux(3)+dscf(ind,7))
+              dm_aux(4) = 0.5_dp*(dm_aux(4)+dscf(ind,8))
+              qspin(ispin) = qspin(ispin) + dm_aux(ispin) * S(ind)
+           enddo
+        enddo
+     enddo
+
+  else
+     
      do ispin = 1,nspin
         qspin(ispin) = 0.0_dp
         do io = 1,no_l
@@ -35,7 +55,8 @@ subroutine print_spin(qspin)
            enddo
         enddo
      enddo
-
+     
+  endif
 #ifdef MPI
      !       Global reduction of spin components
      call globalize_sum(qspin(1:nspin),qtmp(1:nspin))
@@ -68,5 +89,5 @@ subroutine print_spin(qspin)
            endif !cml_p
         endif
      endif
-  endif
+
 end subroutine print_spin
