3bb6bf7c8e2b34dc2524ccd073f8f2b6506a02bb
Alberto Garcia <albertog@icmab.es>
2019-04-11 14:38:34 +0200

    Add interfaces for 'in place' all-reduce operations
    A new module 'm_mpi_inplace' provides interfaces for the 'in place'
    version of mpi_all_reduce, which was implicitly used by Rogeli Grima
    in his work.
    
    The calls might have been re-worked, but the new module will be useful
    in the future to eliminate the need for extra variables and for extra
    copy operations.
    
    As the 'in-place' option is not supported by the 'classic' MPI
    interface contained in mpi_siesta, the new module does not use the
    latter. As side effects, the 'comm' argument is now mandatory, and
    no 'MPI' timing information is provided.

---
+++ Src/m_mpi_inplace.F90
@@ -0,0 +1,77 @@
+!     
+!     Copyright (C) 1996-2016	The SIESTA group
+!     This file is distributed under the terms of the
+!     GNU General Public License: see COPYING in the top directory
+!     or http://www.gnu.org/copyleft/gpl.txt.
+!     See Docs/Contributors.txt for a list of contributors.
+!     
+!> Provides interfaces for in-place all-reduce operations
+!> This operation is not supported by the old-style interfaces
+!> in the mpi_siesta module. Hence, the generic 'mpi' module
+!> is used instead, and the communicator argument is mandatory,
+!> as we cannot access the (possibly redefined) mpi_comm_world
+!> as a default.
+!> Note that (when implemented) a scalar-integer interface in this module
+!> will not be distinguishable from that in m_mpi_utils.
+
+module m_mpi_inplace
+      use precision, only: dp, sp
+#ifdef MPI
+      use mpi
+      implicit none
+      integer, private :: MPIerror
+#else
+      implicit none
+#endif
+      public :: globalize_sum
+      private
+      
+      interface globalize_sum
+       module procedure globalize_sum_inplace_dp
+       module procedure globalize_sum_inplace_v_dp
+       module procedure globalize_sum_inplace_vv_dp
+      end interface
+
+      CONTAINS
+
+!--------------------------------------------------------------
+!     In-place versions of 'dp' interfaces
+      
+      subroutine Globalize_sum_inplace_dp(var,comm)
+      real(dp), intent(inout) :: var
+      integer, intent(in)     :: comm
+
+#ifdef MPI
+      call MPI_AllReduce(MPI_IN_PLACE,var,1,MPI_double_precision, &
+                         MPI_sum,comm,MPIerror)
+#else
+      ! do nothing
+#endif
+      end subroutine Globalize_sum_inplace_dp
+
+      subroutine Globalize_sum_inplace_v_dp(var,comm)
+      real(dp), intent(inout), dimension(:)  :: var
+      integer, intent(in)                    :: comm
+
+#ifdef MPI
+      call MPI_AllReduce(MPI_IN_PLACE,var(1),size(var),MPI_double_precision, &
+                         MPI_sum,comm,MPIerror)
+#else
+      ! do nothing
+#endif
+      end subroutine Globalize_sum_inplace_v_dp
+
+      subroutine Globalize_sum_inplace_vv_dp(var,comm)
+      real(dp), intent(inout), dimension(:,:)  :: var
+      integer, intent(in), optional :: comm
+
+#ifdef MPI
+      call MPI_AllReduce(MPI_IN_PLACE,var(1,1),size(var),MPI_double_precision, &
+                        MPI_sum,comm,MPIerror)
+#else
+      ! do nothing
+#endif
+      end subroutine Globalize_sum_inplace_vv_dp
+
+    end module m_mpi_inplace
+
