bf04719fb14ddd0e4d65a9712d597631cc9bc8a5
Nick Papior <nickpapior@gmail.com>
2019-06-17 14:42:50 +0200

    Added checks for -fdf flags on command line
    One would think that %include statements works.
    However, it will not work since the flags in the file
    will be put at the end of the fdf-table.
    
    As such we have made sure that users will not do this
    and thus added error messages.

--- Src/fdf/fdf.F90
+++ Src/fdf/fdf.F90
@@ -1523,12 +1523,43 @@ MODULE fdf
       type(parsed_line), pointer  :: pline
       character(len=MAX_LENGTH)   :: label
 
+      integer :: ierr
+      
       pline => digest(line)
-      label = tokens(pline,1)
-      call destroy(pline)
+      if ( search('%block', pline) == 1 .or. &
+          search('%endblock', pline) == 1 ) then
 
-      call fdf_removelabel(label)
-      call fdf_addline(line)
+        ! We do not allow this in a single line
+        call die('FDF module: fdf_overwrite', 'Error overwriting block (not implemented)',   &
+            THIS_FILE, __LINE__, fdf_err, rc=ierr)
+        
+      else if ( search('%include', pline) == 1 ) then
+
+        ! We do not allow this in a single line
+        call die('FDF module: fdf_overwrite', 'Error overwriting flags from input file (not implemented)',   &
+            THIS_FILE, __LINE__, fdf_err, rc=ierr)
+
+      else if ( search('<', pline) /= -1 ) then
+
+        ! We do not allow this in a single line
+        call die('FDF module: fdf_overwrite', 'Error piping in overwriting (not implemented)',   &
+            THIS_FILE, __LINE__, fdf_err, rc=ierr)
+        
+      else
+        
+        label = tokens(pline,1)
+        call setmorphol(1, 'l', pline)
+        call fdf_removelabel(label)
+
+        ! Add token to the list of fdf-flags
+        call fdf_addtoken(line, pline)
+        if ( fdf_debug ) then
+          write(fdf_log,'(2a)') '---> Overwriting token: ', trim(label)
+        end if
+
+      end if
+
+      call destroy(pline)
       
     END SUBROUTINE fdf_overwrite
 
