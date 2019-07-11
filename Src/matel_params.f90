c15f4fd40e7ef98bc96e80c92ae6e7a3befbbe85
Alberto Garcia <albertog@icmab.es>
2019-07-11 17:02:14 +0200

    Cosmetic changes in matel_ modules
    * Remove extra print statements
    * Remove unused abstract-interface block
    * Update value of pi in matel_params

--- Src/matel_params.f90
+++ Src/matel_params.f90
@@ -1,13 +1,13 @@
 module matel_params
 
-  use precision, only: dp
+  integer, parameter, private :: dp = selected_real_kind(10,100)
   
   integer,          parameter :: NQ        =  1024
   integer,          parameter :: NR        =  NQ
   integer,          parameter :: NRTAB     =  1024
   real(dp),         parameter :: Q2CUT     =  2.50e3_dp
 
-  real(dp), parameter :: PI = 3.141592653589_dp
+  real(dp), parameter :: pi = 3.14159265358979323846264338327950288419716939937510_dp
   real(dp), parameter :: QMAX = 2.0_dp * SQRT( Q2CUT )
   real(dp), parameter :: DQ = QMAX / NQ
   real(dp), parameter :: DR = PI / QMAX
