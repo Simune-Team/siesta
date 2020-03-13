72f866c2b6f12c1d696c9bf3b10beb426683776d
Peter Koval <koval.peter@gmail.com>
2020-03-13 02:40:49 +0100

    adding ci

---
+++ gitlab-runner/docker/ubuntu/18.04/install-siesta-4.0.2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Stop at first error
+set -e
+
+# Init
+siesta_bindir="/siesta/bin"
+siesta_cfgdir="/siesta"
+siesta_srcdir="/siesta/trunk"
+siesta_version="4.0.2"
+vendor="gnu"
+
+# Update the source tree
+cd "${siesta_srcdir}"
+git checkout master
+git clean -fd
+git clean -fdX
+git pull --prune
+git checkout "v${siesta_version}"
+
+# Install selected flavors of SIESTA
+mkdir -p "${siesta_bindir}"
+for flavor in serial mpi; do
+  siesta_builddir="${siesta_srcdir}/Obj/${flavor}"
+  mkdir -p "${siesta_builddir}"
+  cd "${siesta_builddir}"
+  /bin/bash "${siesta_srcdir}/Src/obj_setup.sh"
+  cp "${siesta_cfgdir}/arch.make-siesta-${siesta_version}-${vendor}-${flavor}" \
+    "arch.make"
+  make clean
+  make
+  cp "siesta" "${siesta_bindir}/siesta-${siesta_version}-${flavor}"
+  cd ..
+  rm -rf $OLDPWD
+done
