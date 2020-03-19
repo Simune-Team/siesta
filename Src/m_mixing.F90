17a32e1c440292b76a81e4e263797618477e43aa
Nick Papior <nickpapior@gmail.com>
2020-03-19 13:12:03 +0100

    maint: added polynomial eigenvalue print-out in mixing routine
    This may be used in the future for accelerating the SCF cycles.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- Src/m_mixing.F90
+++ Src/m_mixing.F90
@@ -1979,6 +1979,7 @@ contains
               trim(debug_msg),&
               ' G = ',G,', sum(alpha) = ',sum(coeff), &
               ', alpha = ',coeff
+         call print_polynomial_roots(nh-1, coeff)
       end if
 
 
@@ -2053,6 +2054,7 @@ contains
               trim(debug_msg), ' G = ', G, &
               ', sum(coeff) = ',sum(coeff), &
               ', coeff = ',coeff
+         call print_polynomial_roots(nh-1, coeff)
       end if
 
 
@@ -2087,6 +2089,52 @@ contains
 !$OMP end parallel
 
     end subroutine mixing_broyden
+
+    subroutine print_polynomial_roots(n, coeff)
+      use parallel, only: IONode
+
+      integer, intent(in) :: n
+      real(dp), intent(in) :: coeff(n+1)
+
+      ! Even for a history of 100 this should fit the stack...
+      real(dp) :: A(n,n)
+      real(dp) :: er(n), ei(n)
+      real(dp), allocatable :: work(:)
+      integer :: i, lwork
+
+      if ( n <= 0 ) return
+
+      ! First we create the companion matrix
+      A(:,:) = 0._dp
+      do i = 1, n - 1
+        A(i+1, i) = 1._dp
+      end do
+
+      ! Now fill up the last column
+      do i = 1, n
+        A(i,n) = A(i,n) - coeff(i) / coeff(n+1)
+      end do
+
+      ! Work-size query
+      lwork = -1
+      call dgeev('N', 'N', n, A, n, er, ei, &
+          A, n, A, n, & ! VL, LDVL, VR, LDVR (not referenced)
+          er, lwork, i)
+
+      ! Get work-size
+      lwork = max(3*n, nint(er(1)))
+      allocate(work(lwork))
+      call dgeev('N', 'N', n, A, n, er, ei, &
+          A, n, A, n, & ! VL, LDVL, VR, LDVR (not referenced)
+          work, lwork, i)
+      deallocate(work)
+
+      if ( i == 0 ) then
+        write(*,'(2a,100(tr2,e10.4,'','',e10.4))') &
+            trim(debug_msg), ' f(x)[coeff] == 0 : ', cmplx(er,ei,dp)
+      end if
+
+    end subroutine print_polynomial_roots
 
   end subroutine mixing_calc_next
 
