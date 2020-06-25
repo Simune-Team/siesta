a8937a7f0969b89b1017c439e6eb94a9cf51378f
Nick Papior <nickpapior@gmail.com>
2020-06-25 14:48:38 +0200

    enh: added vacuum dipole correction
    Now one can calculate the vacuum dipole from 2 methods:
    
    1. The original dipole method (now also with a center).
    2. By looking at a vacuum region and determining the electric
       field from the FFT solution of the Poisson equation.
    
    In all *regular* calculations these are equivalent.
    
    However, when one uses gates one have to be really careful
    on how to calculate the true dipole that counters the system.
    This is because a charged gate does not enter in the calculation
    of the center of coordinates and thus one can easily get *wrong*
    dipoles. The charged gate ensures there is a field between the gate
    and the system. But the dipole position will only be taking into account
    the system and thus the contribution of the gate will have a much larger
    weight.
    
    This commit fixes this by allowing one to choose a vacuum point where
    one wishes the electric field to be exactly 0.
    The method is simply doing numerical differentiation along the given dipole
    direction (currently only 1 direction is allowed). In the pure vacuum
    this should be a straight line.
    To decipher when one is not in the vacuum region we use a threshhold in
    the change of the electric field. I.e. if the electric field changes by more than
    the tolerance the line is considered to be parabolic and we are not in a vacuum region.
    Another reason for using this is that it is agnostic about the placement of the dipole.
    It will correct the *global* induced dipole that the FFT adds to the potential.
    
    The actual field calculated is the minimum electric field in the
    region of the vacuum.
    We choose the minimum since that is stable and should be more stable than
    calculating the average (which then depends on the tolerance).
    
    I have added a test which shows how this may be used. h2o_2_dipol_gate
    Where if one changes to "Dipole charge" one will get the wrong results.

--- Src/dipole.F90
+++ Src/dipole.F90
@@ -19,40 +19,58 @@
 !! This module is highly connected with the E-field module
 module dipole_m
 
+  use precision, only: dp
+
   implicit none
 
   private
 
   integer, parameter, public :: DIPOLECORR_NONE = 0
   integer, parameter, public :: DIPOLECORR_CHARGE = 1
-  integer, public :: DIPOLECORR = DIPOLECORR_NONE
+  integer, parameter, public :: DIPOLECORR_VACUUM = 2
+  integer, public, save :: DIPOLECORR = DIPOLECORR_NONE
 
   public :: init_dipole_correction
   public :: get_dipole_center
   public :: dipole_charge
+  public :: dipole_potential
+
   public :: get_field_from_dipole
   public :: get_dipole_from_field
+
+  type :: dipole_vacuum_dir_t
+    logical :: use_xyz = .false.
+    real(dp) :: xyz(3) = 0._dp
+    real(dp) :: dir(3) = 0._dp
+    real(dp) :: dE_tol = 0._dp
+  end type dipole_vacuum_dir_t
+
+  !< Determine position for vacuum dipole
+  type(dipole_vacuum_dir_t), public, save :: dip_vacuum
 
 contains
 
   !< Read options for the dipole correction
-  subroutine init_dipole_correction(nbcell, acting_efield)
+  subroutine init_dipole_correction(nbcell, bcell, acting_efield)
     use parallel,     only: ionode
-    use siesta_cml,   only: cml_p, cmlAddParameter, mainXML
+    use siesta_cml
     use fdf
     use units,        only: Ang, eV
     use m_cite, only: add_citation
     use m_char, only: lcase
-    use intrinsic_missing, only: vnorm
+    use intrinsic_missing, only: vnorm, vec_proj_sca
 
     !< number of bulk cell-directions
     integer, intent(in) :: nbcell
+    real(dp), intent(in) :: bcell(3,nbcell)
     !< Whether there is an intrinsic electric field
     !!
     !! In case the user request a dipole correction,
     !! this will *always* be true on exit.
     logical, intent(inout) :: acting_efield
 
+    real(dp) :: proj
+    integer :: i
     character(len=32) :: dipole_str
     logical :: old_slabdipole
 
@@ -65,7 +83,7 @@ contains
     end if
 
     select case ( trim(dipole_str) )
-    case ( "true", "t", ".true.", "yes", "slab", "chain", "molecule" )
+    case ( "true", "t", ".true.", "yes", "slab", "chain", "molecule", "charge" )
       ! All these options reflect a dipole correction
       ! based on the original implementation
       ! One *may* use the Dipole.Center block to specify the
@@ -73,17 +91,18 @@ contains
       DIPOLECORR = DIPOLECORR_CHARGE
     case ( "none", "no", "n", "false", "f", ".false." )
       DIPOLECORR = DIPOLECORR_NONE
+    case ( "vacuum" )
+      DIPOLECORR = DIPOLECORR_VACUUM
     case default
       print *, trim(dipole_str)
       call die("init_dipole: Could not determine the dipole option &
-          &[slab, chain, molecule, none]")
+          &[slab, chain, molecule, charge, vacuum, none]")
     end select
 
     ! Further, remove dipole corrections if this is a bulk calculation
-    select case ( nbcell )
-    case ( 3 )
+    if ( nbcell == 3 ) then
       DIPOLECORR = DIPOLECORR_NONE
-    end select
+    end if
 
     select case ( DIPOLECORR )
     case ( DIPOLECORR_NONE )
@@ -96,6 +115,7 @@ contains
             'efield: This is only for backwards compatibility!'
         write(6,*) ! newline
       end if
+
     case ( DIPOLECORR_CHARGE )
       if (cml_p) &
           call cmlAddParameter( xf=mainXML, name='Dipole.Correction', &
@@ -107,6 +127,40 @@ contains
             'dipole: A dipole layer will be introduced in the vacuum', &
             'dipole: region to compensate the system dipole'
       end if
+      acting_efield = .true.
+
+    case ( DIPOLECORR_VACUUM )
+      if (cml_p) &
+          call cmlAddParameter( xf=mainXML, name='Dipole.Correction', &
+          value="vacuum", &
+          dictRef="siesta:dipole_correction")
+
+      ! Retrieve vacuum information
+      call get_vacuum(nbcell, bcell, dip_vacuum)
+
+      if ( IONode ) then
+        write(6,'(/,(a))') &
+            'dipole: A dipole layer will be introduced in the vacuum', &
+            'dipole: region to compensate the vacuum field'
+        write(6,'(a,3(tr1,f12.6))') "dipole: Dipole direction", dip_vacuum%dir
+        write(6,'(a,tr1,f12.6)') "dipole: Field tolerance [eV/Ang/e]", dip_vacuum%dE_tol/eV*Ang
+        if ( dip_vacuum%use_xyz ) then
+          write(6,'(a,3(tr1,f12.3))') "dipole: Vacuum plane point [Ang]", dip_vacuum%xyz/Ang
+        else
+          write(6,'(a)') "dipole: Vacuum plane point automatically determined"
+        end if
+        write(6,*) ! newline
+      end if
+
+      ! Check if the direction is orthogonal to the bulk lattice vectors
+      do i = 1, nbcell
+        proj = VEC_PROJ_SCA(bcell(:,i), dip_vacuum%dir)
+        if ( abs(proj) > 1.e-7_dp ) then
+          call die("dipole: Dipole direction (vacuum plane normal vector) &
+              &must be orthogonal to the bulk lattice vectors.")
+        end if
+      end do
+
       acting_efield = .true.
     end select
 
@@ -126,7 +180,6 @@ contains
   !! I.e. if users want a specific coordinate for the dipole placement
   !! one should not *trust* the average dipole.
   subroutine get_dipole_center(ucell, na_u, xa, x0)
-    use precision, only: dp
     use sys, only: die
     use fdf
 
@@ -186,10 +239,127 @@ contains
 
   end subroutine get_dipole_center
 
+  !< Return direction and coordinate for vacuum region
+  subroutine get_vacuum(nbcell, bcell, dip_vacuum)
+    use siesta_cml
+    use sys, only: die
+    use fdf
+    use m_char, only: lcase
+    use intrinsic_missing, only: vnorm
+
+    integer, intent(in) :: nbcell
+    real(dp), intent(in) :: bcell(3,nbcell)
+    type(dipole_vacuum_dir_t), intent(inout) :: dip_vacuum
+
+    ! Local variables
+    type(block_fdf) :: bfdf
+    type(parsed_line), pointer :: pline => null()
+    character(len=64) :: unit, name
+    real(dp) :: cfactor
+    integer :: ix
+    ! The vacuum information has 3 quantities
+    !  direction
+    !  tolerance (have a default)
+    ! all must be found, otherwise we die
+    logical :: found(2)
+    external :: cross
+
+    if ( .not. fdf_block("Dipole.Vacuum", bfdf) ) then
+      call die("dipole: Dipole.Vacuum not present but requested, &
+          &please fix input.")
+    end if
+
+    dip_vacuum%use_xyz = .false.
+    found(:) = .false.
+
+    do while ( fdf_bline(bfdf, pline) )
+
+      ! First string is the value
+      name = lcase(fdf_bnames(pline, 1))
+
+      select case ( trim(name) )
+      case ( "direction" )
+
+        found(1) = .true.
+
+        do ix = 1 , 3
+          dip_vacuum%dir(ix) = fdf_bvalues(pline,ix)
+        end do
+
+      case ( "point" )
+
+        dip_vacuum%use_xyz = .true.
+
+        unit = fdf_bnames(pline, 2)
+        cfactor = fdf_convfac(unit, "Bohr")
+        do ix = 1 , 3
+          dip_vacuum%xyz(ix) = fdf_bvalues(pline,ix) * cfactor
+        end do
+
+      case ( "tolerance", "tol" )
+
+        found(2) = .true.
+        unit = fdf_bnames(pline, 2)
+        cfactor = fdf_convfac(unit, "Ry/Bohr/e")
+        dip_vacuum%dE_tol = fdf_bvalues(pline, 1) * cfactor
+
+      end select
+
+    end do
+
+    call fdf_bclose(bfdf)
+
+    if ( .not. found(1) ) then
+      if ( nbcell == 2 ) then
+        ! we can actually figure out the direction our selves
+        call cross(bcell(1,1), bcell(1,2), dip_vacuum%dir)
+        found(1) = .true.
+      end if
+    end if
+
+    ! Normalize direction
+    dip_vacuum%dir(:) = dip_vacuum%dir(:) / vnorm(dip_vacuum%dir)
+
+    if ( .not. found(2) ) then
+      ! Assign default value of tolerance
+      ! This is equivalent to 1e-4 eV/Ang/e
+      dip_vacuum%dE_tol = 3.88937962930779e-6_dp
+    end if
+
+    ! Check if all are found
+    if ( .not. found(1) ) then
+      call die("dipole: Dipole.Vacuum did not contain direction")
+    end if
+
+    if ( cml_p ) then
+      call cmlStartPropertyList(xf=mainXML, &
+          dictRef='siesta:dipole_vacuum', &
+          title='Potential field calculation in vacuum')
+      if ( dip_vacuum%use_xyz ) then
+        call cmlAddProperty(xf=mainXML, &
+            dictref='siesta:dipole_vacuum_point', &
+            title='Vacuum point', value="auto")
+      else
+        call cmlAddProperty(xf=mainXML, &
+            dictref='siesta:dipole_vacuum_point', &
+            units="siestaUnits:Bohr", &
+            title='Vacuum point', value=dip_vacuum%xyz)
+      end if
+      call cmlAddProperty(xf=mainXML, &
+          dictref='siesta:dipole_vacuum_direction', &
+          title='Dipole direction', value=dip_vacuum%dir)
+      call cmlAddProperty(xf=mainXML, &
+          dictref='siesta:dipole_vacuum_tolerance', &
+          units='siestaUnits:Ry_Bohr_e', &
+          title='Field tolerance for vacuum', value=dip_vacuum%dE_tol)
+      call cmlEndPropertyList(xf=mainXML)
+    end if
+
+  end subroutine get_vacuum
+
 
   !< Calculate the field resulting from a dipole
   function get_field_from_dipole(dipole, cell) result(efield)
-    use precision, only: dp
     use units, only: pi
     real(dp), intent(in)  :: dipole(3)
     real(dp), intent(in)  :: cell(3,3)
@@ -204,7 +374,6 @@ contains
 
   !< Calculate the dipole that creates a given field
   function get_dipole_from_field(efield, cell) result(dipole)
-    use precision, only: dp
     use units, only: pi
     real(dp), intent(in)  :: efield(3)
     real(dp), intent(in)  :: cell(3,3)
@@ -241,7 +410,7 @@ contains
 ! dipole in atomic units (electrons*Bohr)
 ! ********************************************************************
 
-    use precision,  only : dp, grid_p
+    use precision,  only : grid_p
     use sys,        only : die
     use mesh,       only : meshLim
 #ifdef MPI
@@ -342,5 +511,269 @@ contains
 #endif
 
   end subroutine dipole_charge
+
+
+  subroutine dipole_potential(cell, na_u, isa, xa, ntm, ntml, nsm, dip_vacuum, V, dipole)
+! ********************************************************************
+! Calculates the dipole field in the Vacuum region
+! This routine only works for FFT calculated Hartree potentials
+! since any dipoles will induce a compensating field in the solution.
+! This method will calculate the resulting dipole by averaging in a vacuum
+! region where the double derivative is below a given tolerance
+! Written by N.Papior. June 2020.
+! Requires distributed V matrix using a 2D grid of processors.
+! *********** INPUT **************************************************
+! real*8  cell(3,3)     : Unit cell vectors
+! integer na_u          : Atoms in unit cell
+! integer isa(na_u)     : Species index for atom
+! real*8  xa(3,na_u)    : Atomic coordinates
+! integer ntm(3)        : Global number of divisions of each lattice vector
+! integer ntml(3)       : Local number of divisions of each lattice vector
+! integer nsm           : Number of sub-points for each mesh point
+! dipole_vacuum_dir_t
+!     dip_vacuum        : type containing vacuum information (dipole direction)
+!                         and the tolerance for the electric field
+! real*?  V(*ntml)      : Hartree potential as calculated from FFT
+! *********** OUTPUT *************************************************
+! real*8 dipole(3)      : A global dipole that creates a potential ramp
+! *********** UNITS **************************************************
+! cell, xa  in atomic units (Bohr)
+! dE_tol  in atomic units (Ry/Bohr/e) (electric field change)
+! V     in atomic units (Ry)
+! dipole in atomic units (Ry*Bohr)
+! ********************************************************************
+!
+!  Modules
+!
+    use precision,  only : grid_p
+    use parallel,   only : Node
+    use sys,        only : die
+    use mesh,       only : meshLim
+    use atmfuncs,     only : rcut
+#ifdef MPI
+    use mpi_siesta
+#endif
+    use units, only: Pi, eV, Ang
+    use intrinsic_missing, only: MODP, vnorm
+
+    real(dp), intent(in) :: cell(3,3)
+    integer, intent(in) :: na_u, isa(na_u)
+    real(dp), intent(in) :: xa(3,na_u)
+    integer, intent(in) :: ntm(3), ntml(3), nsm
+    type(dipole_vacuum_dir_t), intent(in) :: dip_vacuum
+    real(grid_p), intent(in) :: V(ntml(1),ntml(2),ntml(3))
+    real(dp), intent(out) :: dipole(3)
+
+    real(dp), external :: volcel
+    external :: reclat
+
+    ! Internal variables and arrays
+    real(dp) :: rc, rc_dplane, xfrac, field, rcell(3,3)
+    real(dp) :: xmin, xmax, dplane
+    logical :: valid_x0
+    integer :: ix, i0, is, ia, idir
+    integer :: i1, i2, i3, i, offset
+    real(dp), allocatable :: Vavg(:), E(:), dE(:)
+
+#ifdef MPI
+    integer :: MPIerror
+#endif
+
+    ! Figure out which lattice vector dir corresponds to
+    idir = 0
+    do ix = 1, 3
+      xfrac = dot_product(cell(:,ix), dip_vacuum%dir) / vnorm(cell(:,ix))
+      if ( abs(xfrac) > 0.001_dp ) then
+        ! We are having at least a projection of 0.1% onto this
+        ! lattice vector
+        if ( idir == 0 ) then
+          idir = ix
+        else
+          print *, "Direction 1: ", idir
+          print *, "Direction 2: ", ix
+          call die("dipole_from_potential: found multiple directions of &
+              &parallel lattice vectors to the dipole direction, this is not functional")
+        end if
+      end if
+    end do
+    if ( idir == 0 ) then
+      call die("dipole_from_potential: no direction was found parallel to the dipole &
+          &direction, how is this possible?")
+    end if
+
+    ! Find the origin of a shifted cell, with the system centered in it
+    ! This is done at every call, because of possible atomic movements
+
+    ! Find reciprocal unit cell and distance between lattice planes
+    call reclat(cell, rcell, 0)
+
+    dplane = 1.0_dp / sqrt(dot_product(rcell(:,idir), rcell(:,idir)))
+    xmin = 1.0e30_dp
+    xmax = -1.0e30_dp
+    do ia = 1 , na_u
+      is = isa(ia)
+      rc = rcut(is,0)
+      rc_dplane = rc / dplane
+      ! Only take into account the dipole direction
+      xfrac = dot_product(xa(:,ia), rcell(:,idir))
+      xmin = min( xmin, xfrac - rc_dplane )
+      xmax = max( xmax, xfrac + rc_dplane )
+    end do
+    ! Now we have min and max along the dipole direction
+
+    if ( dip_vacuum%use_xyz ) then
+      ! Find vacuum position
+      rc = dot_product(dip_vacuum%xyz, rcell(:,idir))
+      if ( xmin <= rc .and. rc <= xmax ) then
+        call die("dipole_potential: Vacuum point lies in atomic range.")
+      end if
+    else
+      rc = (xmin + xmax) / 2 - 0.5_dp
+    end if
+    i0 = MODP(nint(rc*ntm(idir)) + 10 * ntm(idir), ntm(idir))
+
+    ! Initialize calculated field
+    ! We will finally convert to dipole
+    field = 0.0_dp
+    dipole(:) = 0.0_dp
+
+    ! Some processor *must* have the contribution
+    ! distribute ghost-layers
+
+    allocate(Vavg(ntm(idir)))
+    Vavg(:) = 0._dp
+
+    offset = (meshLim(1,idir)-1)*nsm
+
+    ! Calculate average for all planes with normal along dir
+    select case ( idir )
+    case ( 1 )
+      ! Calculate plane average constant
+      rc = 1._dp / (ntm(2) * ntm(3))
+      do i3 = 1 , ntml(3)
+        do i2 = 1 , ntml(2)
+          do i1 = 1 , ntml(1)
+            i = offset + i1
+            Vavg(i) = Vavg(i) + V(i1,i2,i3)
+          end do
+        end do
+      end do
+    case ( 2 )
+      rc = 1._dp / (ntm(1) * ntm(3))
+      do i3 = 1 , ntml(3)
+        do i2 = 1 , ntml(2)
+          i = offset + i2
+          do i1 = 1 , ntml(1)
+            Vavg(i) = Vavg(i) + V(i1,i2,i3)
+          end do
+        end do
+      end do
+    case ( 3 )
+      rc = 1._dp / (ntm(1) * ntm(2))
+      do i3 = 1 , ntml(3)
+        i = offset + i3
+        do i2 = 1 , ntml(2)
+          do i1 = 1 , ntml(1)
+            Vavg(i) = Vavg(i) + V(i1,i2,i3)
+          end do
+        end do
+      end do
+    end select
+
+    ! We will do two averages
+    ! 1. number of elements in plane (current rc, calculated above)
+    ! 2. Reduce calculations in next step by adding voxel length along
+    !    dipole direction (corrected for here)
+    !    This step removes the rc factor in E(:) calculation
+    rc = ntm(idir) * rc / VNORM(cell(:,idir))
+    do i = offset + 1 , offset + ntml(idir)
+      Vavg(i) = Vavg(i) * rc
+    end do
+
+    ! Now perform a reduction
+    ! There is no need to do this on any other than 1 core
+#ifdef MPI
+    allocate(E(ntm(idir)))
+    E(:) = Vavg(:)
+    call MPI_Reduce(E, Vavg, ntm(idir), MPI_double_precision, MPI_sum, 0, &
+        MPI_Comm_World, MPIerror)
+    deallocate(E)
+#endif
+
+    ! Now perform average calcualtion of field along idir
+    ! This is a 3-point stencil
+    if ( Node == 0 ) then
+
+      allocate(E(ntm(idir)), dE(ntm(idir)))
+      E(1) = 0.5_dp * (Vavg(2) - Vavg(ntm(idir)))
+      dE(1) = Vavg(2) + Vavg(ntm(idir)) - 2 * Vavg(1)
+      do i = 2, ntm(idir) - 1
+        E(i) = 0.5_dp * (Vavg(i+1) - Vavg(i-1))
+        dE(i) = Vavg(i+1) + Vavg(i-1) - 2 * Vavg(i)
+      end do
+      E(ntm(idir)) = 0.5_dp * (Vavg(1) - Vavg(ntm(idir)-1))
+      dE(ntm(idir)) = Vavg(1) + Vavg(ntm(idir)-1) - 2 * Vavg(ntm(idir))
+
+      !$ DEBUG
+      ! write out table with coordinate E and dE
+      !rc = vnorm(cell(:,idir)) / ntm(idir)
+      !do i = 1 , ntm(idir)
+      !  write(8639,'(3(tr1,f14.6))') i * rc, E(i), dE(i)
+      !end do
+
+      ! Now we have everything
+      ! Initialize field sign at i0(idir)
+      i = i0
+      rc = E(i)
+      field = abs(E(i))
+
+      ! step back until we hit a non-valid point
+      do while ( abs(dE(i)) <= dip_vacuum%dE_tol )
+        i = i - 1
+        if ( i < 1 ) i = ntm(idir)
+        if ( i == i0 ) then
+          call die("dipole_from_potential: dE tolerance too high, all points valid")
+        end if
+      end do
+      ! step one line back
+      ! this is ok even if dE(i0) > dip_vacuum%dE_tol
+      ! since we check at the final stage whether it is ok
+      i = i + 1
+
+      ! Find minimum field value in the vacuum
+      ! The reason for the minimum value is that the actual
+      ! vacuum is *far* from the system and in this case
+      ! for a FFT solved Hartree potential there will be
+      ! a local minima in the vacuum point.
+      ! The tolerance is just to cutoff regions of space
+      ! where we have charge.
+      i1 = 0
+      do while ( abs(dE(i)) <= dip_vacuum%dE_tol )
+        field = min(field, abs(E(i)))
+        i = i + 1
+        i1 = i1 + 1
+        ! do it round-robin
+        if ( i > ntm(idir) ) i = 1
+      end do
+
+      ! Get correct sign of field
+      field = sign(field, rc)
+
+      ! We shouldn't use the negative sign here
+      ! since the dipole will be reverted in dhscf
+      dipole(idir) = field * volcel(cell) / (Pi * 2 * 4)
+
+      deallocate(E, dE)
+    end if
+
+    deallocate(Vavg)
+
+#ifdef MPI
+    ! Bcast result from root node
+    call MPI_Bcast(dipole(idir),1,Mpi_Double_Precision, 0, &
+        MPI_Comm_World, MPIerror)
+#endif
+
+  end subroutine dipole_potential
 
 end module dipole_m
